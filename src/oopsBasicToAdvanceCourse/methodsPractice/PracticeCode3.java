196d09069fd64cd063c1f5278b6d9e7c83e7952f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-09 23:56:19 +0530

    add the problem statement for the PracticeCode3.java

--- src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode3.java
+++ src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode3.java
@@ -5,6 +5,14 @@
  *   File: PracticeCode3.java
  */
 
+/*
+ Problem Statement =>
+ 1. Program starts and asks user to enter text.
+ 2. Program calls specific function which take one parameter of String type and returns amount of words in the text.
+ 3. Program prints amount of words to the console.
+  */
+
+
 package oopsBasicToAdvanceCourse.methodsPractice;
 
 import java.util.Scanner;

76ef78dc3c188ba7a0877e346a7aadf8da2fb1b9
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-10 11:21:27 +0530

    add the problem statement for the PracticeCode5.java

--- src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode5.java
+++ src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode5.java
@@ -5,6 +5,17 @@
  *   File: PracticeCode5.java
  */
 
+/*
+ Problem Statement =>
+ 1. Program starts and asks user to specify matrix size. For example if user entered '5' program will generate next matrix.
+ 2. Program asks user this question with options:
+    How much do you want to rotate matrix?
+  - Press 1 to rotate matrix to 90 degrees
+  - Press 2 to rotate matrix to 180 degrees
+  - Press 3 to rotate matrix to 270 degrees
+ 3. When user chosen rotation mode programs prints initial matrix and rotated one.
+  */
+
 package oopsBasicToAdvanceCourse.methodsPractice;
 
 public class PracticeCode5 {

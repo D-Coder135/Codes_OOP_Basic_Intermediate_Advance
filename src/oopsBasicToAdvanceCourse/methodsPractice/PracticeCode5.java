93163996ac0aa6a082a3038b36516809a8b30911
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-15 10:33:41 +0530

    add a public static void method transposeMatrix() along with a parameter

--- src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode5.java
+++ src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode5.java
@@ -91,6 +91,10 @@ public class PracticeCode5 {
         return true;
     }
 
+    public static void transposeMatrix(float[][] matrix) {
+
+    }
+
     public static void rotate90(float[][] matrix) {
         int size = matrix.length - 1;
         for (int i = 0; i < matrix.length; i++) {

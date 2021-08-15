ad7ba333b9dcaa2e066b393972c8f1a7c47848f7
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-15 12:29:41 +0530

    add a initialization statement

--- src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode5.java
+++ src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode5.java
@@ -107,6 +107,7 @@ public class PracticeCode5 {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length / 2; j++) {
                 temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
             }
         }
     }

ea7a18c6d818a5999848265495e827d77429265c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-15 23:29:40 +0530

    add a new java file PracticeCode6.java

---
+++ src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode6.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 15/08/21
+ *   Time: 11:29 PM
+ *   File: PracticeCode6.java
+ */
+
+package oopsBasicToAdvanceCourse.methodsPractice;
+
+public class PracticeCode6 {
+}

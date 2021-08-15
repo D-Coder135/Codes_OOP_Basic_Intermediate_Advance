0e29f93e8325a399c90e67c311b6d300614f20cc
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-15 10:25:10 +0530

    add a print statement

--- src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode5.java
+++ src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode5.java
@@ -80,7 +80,7 @@ public class PracticeCode5 {
                 rotate180(matrix);
             }
             case 3 -> {
-
+                System.out.println("270 degrees rotated:" + System.lineSeparator());
             }
         }
     }

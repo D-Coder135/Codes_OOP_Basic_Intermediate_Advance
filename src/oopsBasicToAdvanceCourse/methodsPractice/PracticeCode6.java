912b3cc90b0933a6fc05cfceb0239b8eed7e078b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-15 23:35:48 +0530

    add the problem statement for the PracticeCode6.java

--- src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode6.java
+++ src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode6.java
@@ -5,6 +5,19 @@
  *   File: PracticeCode6.java
  */
 
+/*
+ Problem Statement =>
+ 1. Program starts and asks user to enter length on array.
+ 2. Program generates array of random integer numbers (use java.util.Random) with array length specified by user.
+ 3. Program creates the second array with size of twice as much as the first array.
+ 4. The first elements in the new array are first the same elements as in the old array. The other half of elements are
+    elements as in the first array multiplied by two.
+
+For example, if the user set initial array length as ten:
+array #1 = {1,3,5,7,10,11,12,15,17,20} (generated randomly)
+array #1 = {1,3,5,7,10,11,12,15,17,20,2,6,10,14,20,22,24,30,34,40}
+  */
+
 package oopsBasicToAdvanceCourse.methodsPractice;
 
 public class PracticeCode6 {

5b152671751000892a77297d7f0e61df78ce51ab
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-16 20:19:15 +0530

    add the main() method along with some logical code inside it

--- src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode7.java
+++ src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode7.java
@@ -14,5 +14,18 @@
 
 package oopsBasicToAdvanceCourse.methodsPractice;
 
+import java.util.Scanner;
+
 public class PracticeCode7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please, enter two numbers separated by space: ");
+        String userInput = sc.nextLine();
+
+        String[] inputArgumentsArray = userInput.split("\\s+");
+        int number1 = Integer.parseInt(inputArgumentsArray[0]);
+        int number2 = Integer.parseInt(inputArgumentsArray[1]);
+
+        System.out.println(gcdRecursive(number1, number2));
+    }
 }

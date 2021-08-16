9f12ca299211621500df268aa941854fe0738a6a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-16 22:50:55 +0530

    add a main() method along with some logic code

--- src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode8.java
+++ src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode8.java
@@ -14,5 +14,15 @@
 
 package oopsBasicToAdvanceCourse.methodsPractice;
 
+import java.util.Scanner;
+
 public class PracticeCode8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please, enter integer: ");
+        int number = sc.nextInt();
+
+        int sumOfDigits = sumDigitsInNumber(number);
+        System.out.println(sumOfDigits);
+    }
 }

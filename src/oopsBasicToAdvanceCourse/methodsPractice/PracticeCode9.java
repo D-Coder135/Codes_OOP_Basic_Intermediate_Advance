745a03daedbff5bec0969631c2db7152d8973bd2
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-16 23:58:10 +0530

    add main() method along with some logical code inside it

--- src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode9.java
+++ src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode9.java
@@ -33,4 +33,18 @@
 package oopsBasicToAdvanceCourse.methodsPractice;
 
 public class PracticeCode9 {
+    public static final String INPUT_DATA = "Login;Name;Email" + System.lineSeparator() +
+            "peterson;Chris Peterson;peterson@outlook.com" + System.lineSeparator() +
+            "james;Derek James;james@gmail.com" + System.lineSeparator() +
+            "jackson;Walter Jackson;jackson@gmail.com" + System.lineSeparator() +
+            "gregory;Mike Gregory;gregory@yahoo.com";
+
+    public static void main(String[] args) {
+        System.out.println("===== Convert 1 demo =====");
+        System.out.println(convert1(INPUT_DATA));
+
+        System.out.println("===== Convert 2 demo =====");
+        System.out.println(convert2(INPUT_DATA));
+
+    }
 }

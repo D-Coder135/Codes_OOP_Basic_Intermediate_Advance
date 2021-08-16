0f5f31fb0e978faf4803989ba453010c3ebbfddd
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-16 22:27:44 +0530

    add the problem statement for the PracticeCode8.java

--- src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode8.java
+++ src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode8.java
@@ -5,6 +5,13 @@
  *   File: PracticeCode8.java
  */
 
+/*
+ Problem Statement =>
+ 1. Program starts and asks user to enter a number.
+ 2. Program calls method which takes int as an argument and returns sum of all digits in this number.
+ 3. Program prints sum of all digits to the console..
+  */
+
 package oopsBasicToAdvanceCourse.methodsPractice;
 
 public class PracticeCode8 {

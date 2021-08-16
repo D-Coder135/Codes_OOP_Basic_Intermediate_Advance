648404017e38159e273116bb9ffc3bf2f5c0c4fa
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-16 23:51:07 +0530

    add the problem statement for the PracticeCode9.java

--- src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode9.java
+++ src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode9.java
@@ -5,6 +5,31 @@
  *   File: PracticeCode9.java
  */
 
+/*
+ Problem Statement =>
+ 1. Program contains methods that can process input string according to requirements below.
+ 2. You have input string like this:
+
+    Login;Name;Email
+
+    Peterson;Chris Peterson:perterson@outlook.com
+    James;Derek James;james@gmail.com
+    Jackson;Walter Jackson;Jackson@gmail.com
+    gregory:Mike Gregory:gregory@yahoo.com
+
+    1. Your program has next method. Which formats input data likes this:
+       peterson ===> peterson@outlook.com
+       jackson ===> jackson@gmail.com
+       gregory ===> gregory@yahoo.com
+
+    2. Your program has next method. Which formats input data like this:
+       Chris Peterson (email: peterson@outlook.com)
+       Derek James (email: james@gmail.com)
+       Walter Jackson (email: jackson@gmail.com)
+       Mike Gregory (email: gregory@yahoo.com)
+  */
+
+
 package oopsBasicToAdvanceCourse.methodsPractice;
 
 public class PracticeCode9 {

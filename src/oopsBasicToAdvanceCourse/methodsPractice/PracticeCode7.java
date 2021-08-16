18b2f0f4dacf300a3c9a4008882c1bf04483a450
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-16 20:17:44 +0530

    add the problem statement inside the PracticeCode7.java

--- src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode7.java
+++ src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode7.java
@@ -5,6 +5,13 @@
  *   File: PracticeCode7.java
  */
 
+/*
+ Problem Statement =>
+ 1. Program starts and asks user to enter two numbers separated by space.
+ 2. Program calls specific method to find the greatest common divisor.
+ 3. Program prints the greatest common divisor to the console.
+  */
+
 package oopsBasicToAdvanceCourse.methodsPractice;
 
 public class PracticeCode7 {

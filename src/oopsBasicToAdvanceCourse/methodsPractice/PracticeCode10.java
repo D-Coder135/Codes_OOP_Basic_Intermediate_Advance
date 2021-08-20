e421b010c3a18bbd8662a4c51f3aba519e2f8860
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-08-20 16:55:07 +0530

    add the problem statement for the PracticeCode10.java

--- src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode10.java
+++ src/oopsBasicToAdvanceCourse/methodsPractice/PracticeCode10.java
@@ -5,6 +5,16 @@
  *   File: PracticeCode10.java
  */
 
+/*
+ Problem Statement =>
+ 1. Program starts and asks user to enter text.
+ 2. Program format text with the next rules:
+    i. all characters in word should become lower case.
+    ii. the first letter in the word should become upper case.
+ 3. Program prints result of formatted text to the console.
+  */
+
+
 package oopsBasicToAdvanceCourse.methodsPractice;
 
 public class PracticeCode10 {

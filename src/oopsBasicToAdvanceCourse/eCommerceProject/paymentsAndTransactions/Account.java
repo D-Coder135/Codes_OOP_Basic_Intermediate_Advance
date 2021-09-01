9cd95bcc46d016ea7aae92ca7308709d8f6ad3ba
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-02 00:12:09 +0530

    add params for the method receiveMoney()

--- src/oopsBasicToAdvanceCourse/eCommerceProject/paymentsAndTransactions/Account.java
+++ src/oopsBasicToAdvanceCourse/eCommerceProject/paymentsAndTransactions/Account.java
@@ -32,8 +32,8 @@ public class Account {
 
     }
 
-    private void recieveMoney() {
-        
+    private void recieveMoney(Account accountFrom, double moneyAmount) {
+
     }
 
     private void addTransaction(Transaction transaction) {

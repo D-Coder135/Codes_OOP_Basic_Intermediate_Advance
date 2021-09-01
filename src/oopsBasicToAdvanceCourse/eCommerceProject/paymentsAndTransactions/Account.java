7080bb161c1a3442f51bf289676a7b1cfcebd001
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-01 18:31:57 +0530

    add a new file Account.java inside the package /oopBasicToAdvance/eCommerceProject/paymentsAndTransactions

---
+++ src/oopsBasicToAdvanceCourse/eCommerceProject/paymentsAndTransactions/Account.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 01/09/21
+ *   Time: 6:31 PM
+ *   File: Account.java
+ */
+
+package oopsBasicToAdvanceCourse.eCommerceProject.paymentsAndTransactions;
+
+public class Account {
+}

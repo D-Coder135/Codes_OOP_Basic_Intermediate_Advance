91b705ffca94f1710bdde0375aa646675c97d645
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-03 19:09:05 +0530

    add a new class Publisher.java inside the package booksManagement

---
+++ src/oopsBasicToAdvanceCourse/eCommerceProject/booksManagement/Publisher.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 03/09/21
+ *   Time: 7:08 PM
+ *   File: Publisher.java
+ */
+
+package oopsBasicToAdvanceCourse.eCommerceProject.booksManagement;
+
+public class Publisher {
+}

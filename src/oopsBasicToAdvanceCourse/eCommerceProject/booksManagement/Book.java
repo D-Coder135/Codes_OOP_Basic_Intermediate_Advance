c1d8ef84b0b7800adfb1d40a9ac10d7aa23f5686
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-03 19:43:44 +0530

    add getter and setter methods for the private field inside the class Book.java

--- src/oopsBasicToAdvanceCourse/eCommerceProject/booksManagement/Book.java
+++ src/oopsBasicToAdvanceCourse/eCommerceProject/booksManagement/Book.java
@@ -29,4 +29,60 @@ public class Book {
 
     public Book() {
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAmountOfPages() {
+        return amountOfPages;
+    }
+
+    public void setAmountOfPages(int amountOfPages) {
+        this.amountOfPages = amountOfPages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Author[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Author[] authors) {
+        this.authors = authors;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public CoverType getCoverType() {
+        return coverType;
+    }
+
+    public void setCoverType(CoverType coverType) {
+        this.coverType = coverType;
+    }
 }

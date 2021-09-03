5e0287df1a101b921f499d9e8d110f7338dca13d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-03 19:06:53 +0530

    add a new class Author.java to the project booksManagement

---
+++ src/oopsBasicToAdvanceCourse/eCommerceProject/booksManagement/Author.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 03/09/21
+ *   Time: 7:06 PM
+ *   File: Author.java
+ */
+
+package oopsBasicToAdvanceCourse.eCommerceProject.booksManagement;
+
+public class Author {
+}

6d36034d8741904560a49cd88735eea6c4f94a56
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-03 19:49:55 +0530

    override the equals() and hashCode() method

--- src/oopsBasicToAdvanceCourse/eCommerceProject/booksManagement/Author.java
+++ src/oopsBasicToAdvanceCourse/eCommerceProject/booksManagement/Author.java
@@ -7,6 +7,8 @@
 
 package oopsBasicToAdvanceCourse.eCommerceProject.booksManagement;
 
+import java.util.Objects;
+
 public class Author {
     private int id;
     private String firstName, lastName;
@@ -18,5 +20,18 @@ public class Author {
     }
 
     public Author() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return id == author.id && Objects.equals(firstName, author.firstName) && Objects.equals(lastName, author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName);
     }
 }

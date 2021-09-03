54764a07cf0dbc8c9cb6163d1b181f7237364234
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-03 23:04:52 +0530

    initialize an object of the class BookService

--- src/oopsBasicToAdvanceCourse/eCommerceProject/booksManagement/Demo.java
+++ src/oopsBasicToAdvanceCourse/eCommerceProject/booksManagement/Demo.java
@@ -17,5 +17,7 @@ public class Demo {
                 new Book(3, "Book_3", new Author[]{new Author(3, "Walter", "Peterson")}, new Publisher(1, "Publisher_1"), 1997, 350, BigDecimal.valueOf(34.99), CoverType.HARDCOVER),
                 new Book(4, "Book_4", new Author[]{new Author(4, "Craig", "Gregory")}, new Publisher(3, "Publisher_3"), 1992, 185, BigDecimal.valueOf(19.99), CoverType.PAPERBACK),
         };
+
+        BookService bookService = new BookService();
     }
 }

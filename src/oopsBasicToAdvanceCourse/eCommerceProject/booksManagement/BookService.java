19665969461328efa6b8ac1f1af9a568203b30d1
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-03 19:59:11 +0530

    add a class BookService.java inside the package booksManagement

---
+++ src/oopsBasicToAdvanceCourse/eCommerceProject/booksManagement/BookService.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 03/09/21
+ *   Time: 7:58 PM
+ *   File: BookService.java
+ */
+
+package oopsBasicToAdvanceCourse.eCommerceProject.booksManagement;
+
+public class BookService {
+}

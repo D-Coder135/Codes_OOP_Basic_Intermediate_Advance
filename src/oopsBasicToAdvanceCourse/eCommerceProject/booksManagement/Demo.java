1ad2a5886923deddae806f64f2b0f2d6e4fc394b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-03 22:47:03 +0530

    add a new class Demo.java inside the package booksManagement

---
+++ src/oopsBasicToAdvanceCourse/eCommerceProject/booksManagement/Demo.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 03/09/21
+ *   Time: 10:46 PM
+ *   File: Demo.java
+ */
+
+package oopsBasicToAdvanceCourse.eCommerceProject.booksManagement;
+
+public class Demo {
+}

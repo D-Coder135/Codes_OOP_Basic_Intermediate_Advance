5675de2d85ce330cc3ea2892613b2958be43a2b7
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-07 18:26:20 +0530

    add some code inside the main() method

--- src/final450DSA/array/I_Reverse.java
+++ src/final450DSA/array/I_Reverse.java
@@ -1,7 +1,19 @@
 package final450DSA.array;
 
+import java.util.Scanner;
+
 public class I_Reverse {
     public static void main(String[] args) {
+        //code
+        Scanner scanner = new Scanner(System.in);
+        int testCases = scanner.nextInt();
 
+        for (int i = 1; i <= testCases; i++) {
+            int size = scanner.nextInt();
+            int array[] = new int[size];
+            for (int j = 0; j < size; j++) {
+                array[j] = scanner.nextInt();
+            }
+        }
     }
 }

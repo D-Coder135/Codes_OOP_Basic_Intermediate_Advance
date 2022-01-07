2d56c67a1e75fe100c09fae219a0cf38e2e89031
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-07 18:45:58 +0530

    add the final code inside the class I_Reverse.java

--- src/final450DSA/array/I_Reverse.java
+++ src/final450DSA/array/I_Reverse.java
@@ -5,15 +5,41 @@ import java.util.Scanner;
 public class I_Reverse {
     public static void main(String[] args) {
         //code
-        Scanner scanner = new Scanner(System.in);
-        int testCases = scanner.nextInt();
-
-        for (int i = 1; i <= testCases; i++) {
-            int size = scanner.nextInt();
-            int[] array = new int[size];
-            for (int j = 0; j < size; j++) {
-                array[j] = scanner.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while (test-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
             }
+            revArray(a, n);
+            printArray(a, n);
+
         }
+
+    }
+
+    public static void printArray(int[] a, int n) {
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void revArray(int[] a, int n) {
+
+        int s = 0;
+        int e = n - 1;
+
+        while (s < e) {
+            int temp = a[s];
+            a[s] = a[e];
+            a[e] = temp;
+            s++;
+            e--;
+        }
+
     }
 }

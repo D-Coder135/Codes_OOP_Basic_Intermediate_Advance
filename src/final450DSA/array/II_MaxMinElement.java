b501768a51356270c8c24efc171396b68a49cc0e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-07 22:13:56 +0530

    add a static inner class Pair

--- src/final450DSA/array/II_MaxMinElement.java
+++ src/final450DSA/array/II_MaxMinElement.java
@@ -1,4 +1,12 @@
 package final450DSA.array;
 
 public class II_MaxMinElement {
+    static class Pair {
+        long first, second;
+
+        public Pair(long first, long second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
 }

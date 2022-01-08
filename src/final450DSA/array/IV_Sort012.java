60afbe6ca1890b0914e09f0656f754d29874d91d
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-08 18:50:19 +0530

    add a public static method swap() of type void

--- src/final450DSA/array/IV_Sort012.java
+++ src/final450DSA/array/IV_Sort012.java
@@ -1,4 +1,11 @@
 package final450DSA.array;
 
 public class IV_Sort012 {
+
+    public static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 }

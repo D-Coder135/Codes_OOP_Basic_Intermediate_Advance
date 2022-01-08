b02b3b34c721981b6e506334df07655531556a69
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-08 18:51:03 +0530

    add a public static method sort012() of type void along with some code inside its body

--- src/final450DSA/array/IV_Sort012.java
+++ src/final450DSA/array/IV_Sort012.java
@@ -8,4 +8,30 @@ public class IV_Sort012 {
         arr[j] = temp;
     }
 
+    public static void sort012(int a[], int n) {
+        // code here
+        int low = 0, mid = 0, high = a.length - 1;
+
+        while (mid <= high) {
+            switch (a[mid]) {
+                case 0: {
+                    swap(a, low, mid);
+                    low++;
+                    mid++;
+                    break;
+                }
+
+                case 1: {
+                    mid++;
+                    break;
+                }
+
+                case 2: {
+                    swap(a, mid, high);
+                    high--;
+                    break;
+                }
+            }
+        }
+    }
 }

82782df5aa1c625c63cc84271112d775e7a6d477
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-08 18:52:05 +0530

    add some fix up to the code

--- src/final450DSA/array/IV_Sort012.java
+++ src/final450DSA/array/IV_Sort012.java
@@ -2,13 +2,13 @@ package final450DSA.array;
 
 public class IV_Sort012 {
 
-    public static void swap(int arr[], int i, int j) {
+    public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
-    public static void sort012(int a[], int n) {
+    public static void sort012(int[] a, int n) {
         // code here
         int low = 0, mid = 0, high = a.length - 1;
 
@@ -19,9 +19,7 @@ public class IV_Sort012 {
                     low++;
                     mid++;
                 }
-                case 1 -> {
-                    mid++;
-                }
+                case 1 -> mid++;
                 case 2 -> {
                     swap(a, mid, high);
                     high--;

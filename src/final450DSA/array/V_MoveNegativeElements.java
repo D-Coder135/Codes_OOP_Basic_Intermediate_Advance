83569dc08f2d212dc3df17c259136472001aece7
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-10 19:32:18 +0530

    add an if() block along with a condition inside it

--- src/final450DSA/array/V_MoveNegativeElements.java
+++ src/final450DSA/array/V_MoveNegativeElements.java
@@ -4,9 +4,15 @@ import java.util.ArrayList;
 
 public class V_MoveNegativeElements {
 
-    public void segregateElements(int[] ignoredArr, int n) {
+    public void segregateElements(int[] arr, int n) {
         // Your code goes here
         ArrayList<Integer> list = new ArrayList<>();
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < 0) {
+
+            }
+        }
     }
 
 }

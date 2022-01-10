f9ec4d3076af8efef28a84d38b247add97c30c34
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-10 19:50:50 +0530

    add the problem statement as comment inside the friend

--- src/final450DSA/array/V_MoveNegativeElements.java
+++ src/final450DSA/array/V_MoveNegativeElements.java
@@ -1,3 +1,16 @@
+/**
+ * Given an unsorted array arr[] of size N having both negative and positive integers. The task is place all negative
+ * element at the end of array without changing the order of positive element and negative element.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input :
+ * N = 8
+ * arr[] = {1, -1, 3, 2, -7, -5, 11, 6 }
+ * Output :
+ * 1  3  2  11  6  -1  -7  -5
+ */
+
 package final450DSA.array;
 
 import java.util.ArrayList;
@@ -22,7 +35,6 @@ public class V_MoveNegativeElements {
             arr[index] = integer;
             index++;
         }
-
     }
 
 }

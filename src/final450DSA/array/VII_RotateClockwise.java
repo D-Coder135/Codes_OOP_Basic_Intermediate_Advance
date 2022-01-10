7f5b44fab5b482b850fed57bf6a1fcafa4e32a8c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-10 21:49:32 +0530

    add some fix up to the code

--- src/final450DSA/array/VII_RotateClockwise.java
+++ src/final450DSA/array/VII_RotateClockwise.java
@@ -18,13 +18,7 @@ public class VII_RotateClockwise {
     public void rotate(int[] arr, int n) {
         int temp = arr[n - 1];
 
-        for (int i = n - 1; i > 0; i--) {
-            arr[i] = arr[i - 1];
-        }
+        System.arraycopy(arr, 0, arr, 1, n - 1);
         arr[0] = temp;
-
-        for (int values : arr) {
-            System.out.println(values);
-        }
     }
 }

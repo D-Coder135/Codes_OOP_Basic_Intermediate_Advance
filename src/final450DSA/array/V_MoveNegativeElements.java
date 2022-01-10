c34869d82d94533f3c2a5d7b540521d743fe1adc
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-10 19:30:08 +0530

    initialize an ArrayList object of type Integer

--- src/final450DSA/array/V_MoveNegativeElements.java
+++ src/final450DSA/array/V_MoveNegativeElements.java
@@ -1,9 +1,12 @@
 package final450DSA.array;
 
+import java.util.ArrayList;
+
 public class V_MoveNegativeElements {
 
     public void segregateElements(int[] ignoredArr, int n) {
         // Your code goes here
+        ArrayList<Integer> list = new ArrayList<>();
     }
 
 }

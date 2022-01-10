eeca16c5eeab4a11f03267f254fe95a4348dd31e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-10 21:41:14 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/array/VII_RotateClockwise.java
+++ src/final450DSA/array/VII_RotateClockwise.java
@@ -1,3 +1,17 @@
+/**
+ * Given an array, rotate the array by one position in clock-wise direction.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * N = 5
+ * A[] = {1, 2, 3, 4, 5}
+ * Output:
+ * 5 1 2 3 4
+ */
+
+
 package final450DSA.array;
 
 public class VII_RotateClockwise {

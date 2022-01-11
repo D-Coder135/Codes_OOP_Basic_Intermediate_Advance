38a7334a36f185c1f4973e6dbea3439cdcb9d929
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-11 23:09:31 +0530

    add the method getMinDiff() of type int along with the required code inside its body

--- src/final450DSA/array/IX_MinimizeTheHeight_II.java
+++ src/final450DSA/array/IX_MinimizeTheHeight_II.java
@@ -20,5 +20,32 @@
 
 package final450DSA.array;
 
+import java.util.Arrays;
+
 public class IX_MinimizeTheHeight_II {
+
+    int getMinDiff(int[] arr, int n, int k) {
+        // code here
+        Arrays.sort(arr);
+        int ans = (arr[n - 1] + k) - (arr[0] + k);
+
+        int tempMax = arr[n - 1] - k;
+
+        int tempMin = arr[0] + k;
+
+        int max, min;
+
+        for (int i = 0; i < n - 1; i++) {
+            max = Math.max(tempMax, arr[i] + k);
+            min = Math.min(tempMin, arr[i + 1] - k);
+
+            if (min >= 0) {
+                ans = Math.min(ans, max - min);
+            } else {
+                min = arr[0] + k;
+            }
+        }
+        return ans;
+    }
+
 }

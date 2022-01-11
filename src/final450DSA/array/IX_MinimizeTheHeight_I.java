31697ed5ebd43c476cfcb405560888e3a366dfc7
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-11 22:33:54 +0530

    call the Arrays.sort() method

--- src/final450DSA/array/IX_MinimizeTheHeight_I.java
+++ src/final450DSA/array/IX_MinimizeTheHeight_I.java
@@ -1,10 +1,15 @@
 package final450DSA.array;
 
+import java.util.Arrays;
+
 public class IX_MinimizeTheHeight_I {
 
     int getMinDiff(int[] arr, int n, int k) {
         // code here
         int minDiff = 0;
+
+        Arrays.sort(arr);
+
         return minDiff;
     }
 

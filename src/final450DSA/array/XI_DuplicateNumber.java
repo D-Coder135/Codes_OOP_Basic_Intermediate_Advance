8b93d3fab886863fe9d47189e07af44c010ddeab
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-12 23:43:20 +0530

    add the problem statemen as comment inside the code

--- src/final450DSA/array/XI_DuplicateNumber.java
+++ src/final450DSA/array/XI_DuplicateNumber.java
@@ -1,3 +1,19 @@
+/**
+ * Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+ * There is only one repeated number in nums, return this repeated number.
+ * You must solve the problem without modifying the array nums and uses only constant extra space.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,3,4,2,2]
+ * Output: 2
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [3,1,3,4,2]
+ * Output: 3
+ */
+
 package final450DSA.array;
 
 public class XI_DuplicateNumber {

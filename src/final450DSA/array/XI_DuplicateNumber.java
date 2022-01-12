2463423f31c1dedaca594aba0d23b6dbdce22bb3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-12 23:53:21 +0530

    initialize an object for a boolean array

--- src/final450DSA/array/XI_DuplicateNumber.java
+++ src/final450DSA/array/XI_DuplicateNumber.java
@@ -19,7 +19,6 @@ package final450DSA.array;
 public class XI_DuplicateNumber {
 
     public int findDuplicate(int[] nums) {
-
+        boolean[] booleanArray = new boolean[nums.length];
     }
-
 }

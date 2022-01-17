039ad91f8d1d567f13c902312f118362b17028b0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-17 17:51:59 +0530

    add the problem statement inside the code as comment

--- src/final450DSA/array/XV_CountInversions.java
+++ src/final450DSA/array/XV_CountInversions.java
@@ -1,3 +1,28 @@
+/**
+ * Given an array of integers. Find the Inversion Count in the array.
+ * <p>
+ * Inversion Count: For an array, inversion count indicates how far (or close) the array is from being sorted.
+ * If array is already sorted then the inversion count is 0. If an array is sorted in the reverse order then the
+ * inversion count is the maximum.
+ * Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: N = 5, arr[] = {2, 4, 1, 3, 5}
+ * Output: 3
+ * Explanation: The sequence 2, 4, 1, 3, 5
+ * has three inversions (2, 1), (4, 1), (4, 3).
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: N = 5
+ * arr[] = {2, 3, 4, 5, 6}
+ * Output: 0
+ * Explanation: As the sequence is already
+ * sorted so there is no inversion count.
+ */
+
+
 package final450DSA.array;
 
 public class XV_CountInversions {

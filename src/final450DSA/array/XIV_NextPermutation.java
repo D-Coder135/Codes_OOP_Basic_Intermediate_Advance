d0fc2195a826e0236d45d55de41ffb67d6818998
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-17 10:33:42 +0530

    add the problem statement inside the code as comment

--- src/final450DSA/array/XIV_NextPermutation.java
+++ src/final450DSA/array/XIV_NextPermutation.java
@@ -1,3 +1,25 @@
+/**
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ * If such an arrangement is impossible, it must rearrange it to the lowest possible order (i.e., sorted in ascending order).
+ * The replacement must be in place and use only constant extra memory.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,3]
+ * Output: [1,3,2]
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [3,2,1]
+ * Output: [1,2,3]
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: nums = [1,1,5]
+ * Output: [1,5,1]
+ */
+
+
 package final450DSA.array;
 
 public class XIV_NextPermutation {

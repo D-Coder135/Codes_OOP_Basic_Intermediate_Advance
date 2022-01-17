51e7a917005eab0b5207f24124c466b0d36ba5c4
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-17 18:06:54 +0530

    add a return statement

--- src/final450DSA/array/XV_CountInversions.java
+++ src/final450DSA/array/XV_CountInversions.java
@@ -35,6 +35,7 @@ package final450DSA.array;
 public class XV_CountInversions {
     static long inversionCount(long arr[], long N) {
         long ans = mergeSort(arr, 0, (int) N - 1);
+        return ans;
     }
 
 }

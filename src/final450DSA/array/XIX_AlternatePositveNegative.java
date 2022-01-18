59b94f83f5c62a419a0d9a2798d5aea561595985
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-18 13:04:42 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/array/XIX_AlternatePositveNegative.java
+++ src/final450DSA/array/XIX_AlternatePositveNegative.java
@@ -5,6 +5,20 @@
  *   File: XIX_AlternatePositveNegative
  */
 
+/**
+ * Given an unsorted array Arr of N positive and negative numbers. Your task is to create an array of alternate
+ * positive and negative numbers without changing the relative order of positive and negative numbers.
+ * Note: Array should start with positive number.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * N = 9
+ * Arr[] = {9, 4, -2, -1, 5, 0, -5, -3, 2}
+ * Output:
+ * 9 -2 4 -1 5 -5 0 -3 2
+ */
+
 package final450DSA.array;
 
 public class XIX_AlternatePositveNegative {

403c74fb1482dddfdc4fa6d16723a94ab13af754
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-18 12:06:45 +0530

    add an object for the class Set<Integer>

--- src/final450DSA/array/XVIII_CommonElements.java
+++ src/final450DSA/array/XVIII_CommonElements.java
@@ -23,11 +23,14 @@
 package final450DSA.array;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class XVIII_CommonElements {
     ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) {
         // code here
         ArrayList<Integer> arrayList = new ArrayList<>();
+        Set<Integer> set1 = new HashSet<>();
         return arrayList;
     }
 }

ce1b966bfb800bf87cdc85c8bb5be3fd64af5cb9
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-18 12:10:17 +0530

    call the remove() method on the set1 object field

--- src/final450DSA/array/XVIII_CommonElements.java
+++ src/final450DSA/array/XVIII_CommonElements.java
@@ -41,6 +41,7 @@ public class XVIII_CommonElements {
         for (int element : C) {
             if (set1.contains(element) && set2.contains(element)) {
                 arrayList.add(element);
+                set1.remove(element);
             }
         }
         return arrayList;

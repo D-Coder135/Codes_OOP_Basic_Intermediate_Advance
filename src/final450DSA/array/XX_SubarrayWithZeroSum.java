cc1b097e154b2ce22ab484cd3c8ba2094664f3cb
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-19 11:50:20 +0530

    call the put method on the map object field

--- src/final450DSA/array/XX_SubarrayWithZeroSum.java
+++ src/final450DSA/array/XX_SubarrayWithZeroSum.java
@@ -35,7 +35,7 @@ public class XX_SubarrayWithZeroSum {
             if (map.containsKey(sum)) {
                 return true;
             } else {
-
+                map.put(sum, 1);
             }
         }
     }

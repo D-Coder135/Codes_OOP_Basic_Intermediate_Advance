1e5ba3d57f5b6c7ed5625d40992de8e47fce6fc4
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-19 12:56:50 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/array/XXI_FactorialLargeNumbers.java
+++ src/final450DSA/array/XXI_FactorialLargeNumbers.java
@@ -5,6 +5,24 @@
  *   File: XXI_FactorialLargeNumbers
  */
 
+/**
+ * Given an integer N, find its factorial.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: N = 5
+ * Output: 120
+ * Explanation : 5! = 1*2*3*4*5 = 120
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: N = 10
+ * Output: 3628800
+ * Explanation :
+ * 10! = 1*2*3*4*5*6*7*8*9*10 = 3628800
+ */
+
+
 package final450DSA.array;
 
 public class XXI_FactorialLargeNumbers {

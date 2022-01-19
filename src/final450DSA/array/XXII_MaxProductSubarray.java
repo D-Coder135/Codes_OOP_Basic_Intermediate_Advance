49731f368f7c4bd7af953be3a416dc37352e31a0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-19 13:45:28 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/array/XXII_MaxProductSubarray.java
+++ src/final450DSA/array/XXII_MaxProductSubarray.java
@@ -5,6 +5,20 @@
  *   File: XXII_MaxProductSubarray
  */
 
+/**
+ * Given an array Arr[] that contains N integers (might be positive, negative or zero). Find the product of the
+ * maximum product subarray.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * N = 5
+ * Arr[] = {6, -3, -10, 0, 2}
+ * Output: 180
+ * Explanation: Subarray with maximum product
+ * is [6, -3, -10] which gives product as 180.
+ */
+
 package final450DSA.array;
 
 public class XXII_MaxProductSubarray {

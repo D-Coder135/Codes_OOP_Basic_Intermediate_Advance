c147949ac80137d28d46eed7ced6ec363a12d78c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-19 23:58:17 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/array/XXIII_LargestConsecutiveSubarray.java
+++ src/final450DSA/array/XXIII_LargestConsecutiveSubarray.java
@@ -5,6 +5,26 @@
  *   File: XXIII_LargestConsecutiveSubarray
  */
 
+/**
+ * Given an array of positive integers. Find the length of the longest sub-sequence such that elements
+ * in the subsequence are consecutive integers, the consecutive numbers can be in any order.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * N = 7
+ * a[] = {2,6,1,9,4,5,3}
+ * Output:
+ * 6
+ * Explanation:
+ * The consecutive numbers here
+ * are 1, 2, 3, 4, 5, 6. These 6
+ * numbers form the longest consecutive
+ * subsquence.
+ * Exampl
+ */
+
 package final450DSA.array;
 
 public class XXIII_LargestConsecutiveSubarray {

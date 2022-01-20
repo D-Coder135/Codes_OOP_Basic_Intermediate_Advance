8af95238981ab01a3c69ff9e223d16c9797b1544
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-20 11:29:44 +0530

    add the problem statement inside the code as comment

--- src/final450DSA/array/XXIV_OccurrenceInArray.java
+++ src/final450DSA/array/XXIV_OccurrenceInArray.java
@@ -5,6 +5,11 @@
  *   File: XXIV_OccurenceInArray
  */
 
+/**
+ * Given an array of size n and a number k, find all elements that appear more than n/k times
+ */
+
+
 package final450DSA.array;
 
 public class XXIV_OccurrenceInArray {

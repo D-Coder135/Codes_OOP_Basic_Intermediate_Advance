5c6784079a81e4387929289cd9f885c52ebc989c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-21 23:30:14 +0530

    add some code as comment along with the error message

--- src/final450DSA/array/XXVI_TripletSum.java
+++ src/final450DSA/array/XXVI_TripletSum.java
@@ -51,4 +51,25 @@ public class XXVI_TripletSum {
         }
         return false;
     }
+
+    //   TLE
+    //   List<Integer> list = new ArrayList<>();
+    //   for(int number : A) {
+    //       if(number < X) {
+    //           list.add(number);
+    //       }
+    //   }
+
+    //   if(list.size() >= 3) {
+    //       for(int i = 0; i < list.size(); i++) {
+    //           for(int j = i + 1; j < list.size(); j++) {
+    //               for(int k = j + 1; k < list.size(); k++) {
+    //                   if(list.get(i) + list.get(j) + list.get(k) == X) {
+    //                   return true;
+    //               }
+    //               }
+    //           }
+    //       }
+    //   }
+    //   return false;
 }

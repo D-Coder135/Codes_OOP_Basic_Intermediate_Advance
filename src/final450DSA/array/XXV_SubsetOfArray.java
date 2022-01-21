f4697c6185813222533822f513736ef9c641f33b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-21 22:40:15 +0530

    add the problem statement inside the code as comment

--- src/final450DSA/array/XXV_SubsetOfArray.java
+++ src/final450DSA/array/XXV_SubsetOfArray.java
@@ -5,6 +5,22 @@
  *   File: XXV_SubsetOfArray
  */
 
+/**
+ * Given two arrays: a1[0..n-1] of size n and a2[0..m-1] of size m. Task is to check whether a2[]
+ * is a subset of a1[] or not. Both the arrays can be sorted or unsorted.
+ * It may be assumed that elements in both array are distinct.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * a1[] = {11, 1, 13, 21, 3, 7}
+ * a2[] = {11, 3, 7, 1}
+ * Output:
+ * Yes
+ * Explanation:
+ * a2[] is a subset of a1[]
+ */
+
 package final450DSA.array;
 
 public class XXV_SubsetOfArray {

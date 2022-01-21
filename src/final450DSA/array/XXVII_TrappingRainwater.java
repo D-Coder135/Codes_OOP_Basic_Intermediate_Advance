fcfd633a9656f12d716edbf695f68de3bc3c82aa
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-22 00:23:02 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/array/XXVII_TrappingRainwater.java
+++ src/final450DSA/array/XXVII_TrappingRainwater.java
@@ -5,6 +5,20 @@
  *   File: XXVII_TrappingRainwater
  */
 
+/**
+ * Given an array arr[] of N non-negative integers representing the height of blocks.
+ * If width of each block is 1, compute how much water can be trapped between the blocks during the rainy season.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * N = 6
+ * arr[] = {3,0,0,2,0,4}
+ * Output:
+ * 10
+ */
+
 package final450DSA.array;
 
 public class XXVII_TrappingRainwater {

b5f1de53b69ef5b89e70779a488e3bc0896f7768
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-21 23:25:38 +0530

    add another for loop till list.size()

--- src/final450DSA/array/XXVI_TripletSum.java
+++ src/final450DSA/array/XXVI_TripletSum.java
@@ -39,7 +39,9 @@ public class XXVI_TripletSum {
         List<Integer> list = new ArrayList<>(set);
 
         for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
 
+            }
         }
     }
 }

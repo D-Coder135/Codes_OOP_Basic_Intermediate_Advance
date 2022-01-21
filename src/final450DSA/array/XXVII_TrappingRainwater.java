988caeda2fa74f94a4aec2f09e5ed1935f435d0e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-22 00:27:36 +0530

    update the values inside the right array

--- src/final450DSA/array/XXVII_TrappingRainwater.java
+++ src/final450DSA/array/XXVII_TrappingRainwater.java
@@ -33,7 +33,7 @@ public class XXVII_TrappingRainwater {
 
         right[n - 1] = arr[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-
+            right[i] = Math.max(right[i + 1], arr[i]);
         }
     }
 }

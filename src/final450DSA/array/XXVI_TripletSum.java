5cd20454a2b6c905379431a15d11192870ed90b8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-21 23:20:19 +0530

    add the problem statement inside the code as comment

--- src/final450DSA/array/XXVI_TripletSum.java
+++ src/final450DSA/array/XXVI_TripletSum.java
@@ -5,6 +5,22 @@
  *   File: XXVI_TripletSum
  */
 
+/**
+ * Given an array arr of size n and an integer X. Find if there's a triplet in the array which sums up to the given
+ * integer X.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * n = 6, X = 13
+ * arr[] = [1 4 45 6 10 8]
+ * Output:
+ * 1
+ * Explanation:
+ * The triplet {1, 4, 8} in
+ * the array sums up to 13.
+ */
+
 package final450DSA.array;
 
 public class XXVI_TripletSum {

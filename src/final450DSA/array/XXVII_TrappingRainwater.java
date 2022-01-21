bc042996a9ea85ab993f8d89ceed4b1b98fa9f7e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-22 00:26:59 +0530

    add a for loop

--- src/final450DSA/array/XXVII_TrappingRainwater.java
+++ src/final450DSA/array/XXVII_TrappingRainwater.java
@@ -27,11 +27,13 @@ public class XXVII_TrappingRainwater {
         int[] right = new int[n];
 
         left[0] = arr[0];
-
         for (int i = 1; i < n; i++) {
             left[i] = Math.max(left[i - 1], arr[i]);
         }
 
         right[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+
+        }
     }
 }

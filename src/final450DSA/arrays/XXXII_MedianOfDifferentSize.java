6ff51ccfd4389bec55045114f938f5aee1ee8b38
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-25 14:55:41 +0530

    add some code to take input from the user and method call inside the main() method body

--- src/final450DSA/arrays/XXXII_MedianOfDifferentSize.java
+++ src/final450DSA/arrays/XXXII_MedianOfDifferentSize.java
@@ -12,5 +12,18 @@ import java.util.Scanner;
 public class XXXII_MedianOfDifferentSize {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[m];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < m; i++) {
+            arr2[i] = scanner.nextInt();
+        }
+
+        System.out.println(calcMedian(arr1, arr2, m, n));
     }
 }

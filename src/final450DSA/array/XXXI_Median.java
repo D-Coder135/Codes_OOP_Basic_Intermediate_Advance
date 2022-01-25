acdaacaebf44f1659eacea2d421cd2ac3b1fabf6
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-25 12:41:54 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/array/XXXI_Median.java
+++ src/final450DSA/array/XXXI_Median.java
@@ -5,6 +5,27 @@
  *   File: XXXI_Median
  */
 
+/**
+ * Given an array arr[] of N integers, calculate the median
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: N = 5
+ * arr[] = 90 100 78 89 67
+ * Output: 89
+ * Explanation: After sorting the array
+ * middle element is the median
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: N = 4
+ * arr[] = 56 67 30 79
+ * Output: 61
+ * Explanation: In case of even number of
+ * elements, average of two middle elements
+ * is the median.
+ */
+
 package final450DSA.array;
 
 public class XXXI_Median {

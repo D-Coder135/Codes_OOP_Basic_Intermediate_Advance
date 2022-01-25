040dad889faeffb577c187cce7b44e423580b154
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-25 15:25:17 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/arrays2D/I_MatrixSpiralTraversal.java
+++ src/final450DSA/arrays2D/I_MatrixSpiralTraversal.java
@@ -5,6 +5,21 @@
  *   File: I_MatrixSpiralTraversal
  */
 
+/**
+ * Given a matrix of size r*c. Traverse the matrix in spiral form.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * r = 4, c = 4
+ * matrix[][] = {{1, 2, 3, 4},
+ * {5, 6, 7, 8},
+ * {9, 10, 11, 12},
+ * {13, 14, 15,16}}
+ * Output:
+ * 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+ */
+
 package final450DSA.arrays2D;
 
 public class I_MatrixSpiralTraversal {

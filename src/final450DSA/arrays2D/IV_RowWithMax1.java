5eeec99b78b77fb53e19b3d4000364858e3b8eca
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-30 14:49:20 +0530

    add a method rowWithMax1s() of type int along with solution code inside its body

--- src/final450DSA/arrays2D/IV_RowWithMax1.java
+++ src/final450DSA/arrays2D/IV_RowWithMax1.java
@@ -8,4 +8,23 @@
 package final450DSA.arrays2D;
 
 public class IV_RowWithMax1 {
+    int rowWithMax1s(int[][] arr, int n, int m) {
+        // code here
+        int max = -1;
+        int c = 0;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 1) {
+                    count++;
+                }
+            }
+            if (c < count) {
+                c = count;
+                max = i;
+            }
+            count = 0;
+        }
+        return max;
+    }
 }

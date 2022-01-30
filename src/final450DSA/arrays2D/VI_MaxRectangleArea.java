d20d0e6897c60162771559c59d3939c56fc0c526
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-30 21:49:55 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/arrays2D/VI_MaxRectangleArea.java
+++ src/final450DSA/arrays2D/VI_MaxRectangleArea.java
@@ -5,6 +5,32 @@
  *   File: VI_MaxRectangleArea
  */
 
+/**
+ * IMPORTANT QUESTION (LEETCODE QUESTION 85) =>
+ * <p>
+ * Given a binary matrix M of size n X m. Find the maximum area of a rectangle formed only of 1s in the given matrix.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * n = 4, m = 4
+ * M[][] = {{0 1 1 0},
+ * {1 1 1 1},
+ * {1 1 1 1},
+ * {1 1 0 0}}
+ * Output: 8
+ * Explanation: For the above test case the
+ * matrix will look like
+ * 0 1 1 0
+ * 1 1 1 1
+ * 1 1 1 1
+ * 1 1 0 0
+ * the max size rectangle is
+ * 1 1 1 1
+ * 1 1 1 1
+ * and area is 4 *2 = 8.
+ */
+
 package final450DSA.arrays2D;
 
 public class VI_MaxRectangleArea {

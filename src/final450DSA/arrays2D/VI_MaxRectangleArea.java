2844743d8e81cf87f4ae6e91a6792101676db55e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-30 21:48:42 +0530

    add a new class VI_MaxRectangleArea.java inside the package final450DSA.arrays2D

---
+++ src/final450DSA/arrays2D/VI_MaxRectangleArea.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 30-01-2022/01/2022
+ *   Time: 09:48 PM
+ *   File: VI_MaxRectangleArea
+ */
+
+package final450DSA.arrays2D;
+
+public class VI_MaxRectangleArea {
+}

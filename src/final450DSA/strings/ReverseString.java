52cc281bb9e0dffb326d21770d6cd83565ea4e59
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-03 18:37:07 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/ReverseString.java
+++ src/final450DSA/strings/ReverseString.java
@@ -5,6 +5,25 @@
  *   File: ReverseString
  */
 
+/**
+ * LEETCODE QUESTION 344 =>
+ * <p>
+ * Write a function that reverses a string. The input string is given as an array of characters s.
+ * <p>
+ * You must do this by modifying the input array in-place with O(1) extra memory.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: s = ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ */
+
 package final450DSA.strings;
 
 public class ReverseString {

f6d1b6624b50666c9c71ab90891910082ac7b923
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-03 11:14:18 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/arrays2D/VIII_KthSmallestElement.java
+++ src/final450DSA/arrays2D/VIII_KthSmallestElement.java
@@ -5,6 +5,23 @@
  *   File: VIII_KthSmallestElement
  */
 
+/**
+ * Given a N x N matrix, where every row and column is sorted in non-decreasing order. Find the kth the smallest element
+ * in the matrix.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * N = 4
+ * mat[][] =     {{16, 28, 60, 64},
+ * {22, 41, 63, 91},
+ * {27, 50, 87, 93},
+ * {36, 78, 87, 94 }}
+ * K = 3
+ * Output: 27
+ * Explanation: 27 is the 3rd smallest element.
+ */
+
 package final450DSA.arrays2D;
 
 public class VIII_KthSmallestElement {

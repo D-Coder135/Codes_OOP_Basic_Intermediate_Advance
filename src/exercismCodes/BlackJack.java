9acca3c3651050560bdb6ca0bac5be71b72be6b5
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-11 21:38:55 +0530

    add some fix up to the code

--- src/exercismCodes/BlackJack.java
+++ src/exercismCodes/BlackJack.java
@@ -1,22 +1,41 @@
-public class Blackjack {
+public class BlackJack {
 
     public int parseCard(String card) {
         throw new UnsupportedOperationException("Please implement the Blackjack.parseCard method");
         switch (card) {
-            case "ace" =>return 11;
-            case "two" =>return 2;
-            case "three" =>return 3;
-            case "four" =>return 4;
-            case "five" =>return 5;
-            case "six" =>return 6;
-            case "seven" =>return 7;
-            case "eight" =>return 8;
-            case "nine" =>return 9;
-            case "ten" =>return 10;
-            case "king" =>return 10;
-            case "jack" =>return 10;
-            case "queen" =>return 10;
-            default =>return 0;
+            case "ace" -> {
+                return 11;
+            }
+            case "two" -> {
+                return 2;
+            }
+            case "three" -> {
+                return 3;
+            }
+            case "four" -> {
+                return 4;
+            }
+            case "five" -> {
+                return 5;
+            }
+            case "six" -> {
+                return 6;
+            }
+            case "seven" -> {
+                return 7;
+            }
+            case "eight" -> {
+                return 8;
+            }
+            case "nine" -> {
+                return 9;
+            }
+            case "ten", "king", "jack", "queen" -> {
+                return 10;
+            }
+            default -> {
+                return 0;
+            }
         }
     }
 

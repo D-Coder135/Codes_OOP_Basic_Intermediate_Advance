1a6489a621a4f4c1ae377938088252b058abb08b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-14 20:10:38 +0530

    add an object of type StringBuilder

--- src/final450DSA/strings/CountAndSay.java
+++ src/final450DSA/strings/CountAndSay.java
@@ -50,6 +50,7 @@ public class CountAndSay {
 
         private String helper(String str) {
             int count = 1;
+            StringBuilder string = new StringBuilder();
 
             for (int i = 0; i < str.length() - 1; i++) {
                 if (str.charAt(i) == str.charAt(i + 1)) {

2414a97270a51b959758b0867b1a9560b8a04d8a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-14 20:06:36 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/CountAndSay.java
+++ src/final450DSA/strings/CountAndSay.java
@@ -5,6 +5,37 @@
  *   File: CountAndSay
  */
 
+/**
+ * LEETCODE QUESTION 38 =>
+ * <p>
+ * The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+ * <p>
+ * countAndSay(1) = "1"
+ * countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a
+ * different digit string.
+ * To determine how you "say" a digit string, split it into the minimal number of groups so that each group is a
+ * contiguous section all the same character. Then for each group, say the number of characters, then say the character.
+ * To convert the saying into a digit string, replace the counts with a number and concatenate every saying.
+ * <p>
+ * Given a positive integer n, return the nth term of the count-and-say sequence.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 1
+ * Output: "1"
+ * Explanation: This is the base case.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: n = 4
+ * Output: "1211"
+ * Explanation:
+ * countAndSay(1) = "1"
+ * countAndSay(2) = say "1" = one 1 = "11"
+ * countAndSay(3) = say "11" = two 1's = "21"
+ * countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+ */
+
 package final450DSA.strings;
 
 public class CountAndSay {

a013d813bd75d462a443751919e1347358282b14
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-14 23:28:42 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/LongestPalindromeSubstring.java
+++ src/final450DSA/strings/LongestPalindromeSubstring.java
@@ -5,6 +5,24 @@
  *   File: LongestPalindromeSubstring
  */
 
+/**
+ * LEETCODE QUESTION 5 =>
+ * <p>
+ * Given a string s, return the longest palindromic substring in s.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: s = "cbbd"
+ * Output: "bb"
+ */
+
+
 package final450DSA.strings;
 
 public class LongestPalindromeSubstring {

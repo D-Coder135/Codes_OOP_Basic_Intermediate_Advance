caa8a057366f975b3bb2ce3cfde36a18a422248f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-14 20:10:00 +0530

    add an if() block

--- src/final450DSA/strings/CountAndSay.java
+++ src/final450DSA/strings/CountAndSay.java
@@ -52,7 +52,9 @@ public class CountAndSay {
             int count = 1;
 
             for (int i = 0; i < str.length() - 1; i++) {
-
+                if (str.charAt(i) == str.charAt(i + 1)) {
+                    count++;
+                }
             }
             return null;
         }

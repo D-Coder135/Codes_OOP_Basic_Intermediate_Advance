be9a38f78136e4d3c74ce3f5cc67ed2a11decbbd
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-15 18:35:41 +0530

    add a for loop inside the for loop

--- src/final450DSA/strings/LongestRepeatingSubsequence.java
+++ src/final450DSA/strings/LongestRepeatingSubsequence.java
@@ -23,7 +23,9 @@ public class LongestRepeatingSubsequence {
             }
 
             for (int i = 1; i <= length; i++) {
+                for (int j = 1; j <= length; j++) {
 
+                }
             }
         }
     }

6244d3500238d5465107cd25313eede767d9da58
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-15 19:03:13 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/LongestRepeatingSubsequence.java
+++ src/final450DSA/strings/LongestRepeatingSubsequence.java
@@ -5,6 +5,45 @@
  *   File: LongestRepeatingSubsequence
  */
 
+/**
+ * LONGEST REPEATING SUBSEQUENCE GFG =>
+ * <p>
+ * Given a string str, find the length of the longest repeating subsequence such that it can be found twice in the
+ * given string. The two identified subsequences A and B can use the same ith character from string str if and only if
+ * that ith character has different indices in A and B.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * str = "axxxy"
+ * Output: 2
+ * Explanation:
+ * The given array with indexes looks like
+ * a x x x y
+ * 0 1 2 3 4
+ * <p>
+ * The longest subsequence is "xx".
+ * It appears twice as explained below.
+ * <p>
+ * subsequence A
+ * x x
+ * 0 1  <-- index of subsequence A
+ * ------
+ * 1 2  <-- index of str
+ * <p>
+ * <p>
+ * subsequence B
+ * x x
+ * 0 1  <-- index of subsequence B
+ * ------
+ * 2 3  <-- index of str
+ * <p>
+ * We are able to use character 'x'
+ * (at index 2 in str) in both subsequences
+ * as it appears on index 1 in subsequence A
+ * and index 0 in subsequence B.
+ */
+
 package final450DSA.strings;
 
 public class LongestRepeatingSubsequence {

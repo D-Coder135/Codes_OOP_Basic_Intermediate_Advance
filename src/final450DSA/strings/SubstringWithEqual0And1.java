de0b8dc39f31cf4669b89cc12cdcf3f63ee87090
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-16 21:24:49 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/SubstringWithEqual0And1.java
+++ src/final450DSA/strings/SubstringWithEqual0And1.java
@@ -5,6 +5,25 @@
  *   File: SubstringWithEqual0And1
  */
 
+/**
+ * Count Number Of Substrings with equal 0 and 1 GFG =>
+ * <p>
+ * Given a binary string str of length N, the task is to find the maximum count of consecutive substrings str can be
+ * divided into such that all the substrings are balanced i.e. they have equal number of 0s and 1s. If it is not
+ * possible to split str satisfying the conditions then print -1.
+ * Example:
+ * <p>
+ * Input: str = “0100110101”
+ * Output: 4
+ * The required substrings are “01”, “0011”, “01” and “01”.
+ * <p>
+ * Input: str = “0111100010”
+ * Output: 3
+ * <p>
+ * Input: str = “0000000000”
+ * Output: -1
+ */
+
 package final450DSA.strings;
 
 public class SubstringWithEqual0And1 {

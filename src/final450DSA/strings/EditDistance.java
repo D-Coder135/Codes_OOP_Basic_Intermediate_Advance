eec354457f7d3c9493310e92fad5e5eb0b82b95c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-16 22:54:47 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/EditDistance.java
+++ src/final450DSA/strings/EditDistance.java
@@ -5,6 +5,40 @@
  *   File: EditDistance
  */
 
+/**
+ * GFG & LEETCODE QUESTION 72 =>
+ * <p>
+ * Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+ * <p>
+ * You have the following three operations permitted on a word:
+ * <p>
+ * Insert a character
+ * Delete a character
+ * Replace a character
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: word1 = "horse", word2 = "ros"
+ * Output: 3
+ * <p>
+ * Explanation:
+ * horse -> rorse (replace 'h' with 'r')
+ * rorse -> rose (remove 'r')
+ * rose -> ros (remove 'e')
+ * Example 2:
+ * <p>
+ * Input: word1 = "intention", word2 = "execution"
+ * Output: 5
+ * <p>
+ * Explanation:
+ * intention -> inention (remove 't')
+ * inention -> enention (replace 'i' with 'e')
+ * enention -> exention (replace 'n' with 'x')
+ * exention -> exection (replace 'n' with 'c')
+ * exection -> execution (insert 'u')
+ */
+
 package final450DSA.strings;
 
 public class EditDistance {

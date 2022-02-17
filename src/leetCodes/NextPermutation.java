e2e9bdac1509ce770028d46888c2ecd0bc4fd058
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-17 14:57:13 +0530

    add a new class NextPermutation.java inside the package leetCodes

---
+++ src/leetCodes/NextPermutation.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 17-02-2022/02/2022
+ *   Time: 02:56 PM
+ *   File: NextPermutation
+ */
+
+package leetCodes;
+
+public class NextPermutation {
+}

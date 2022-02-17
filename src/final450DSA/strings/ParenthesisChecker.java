7e9bdd1365c747563c694e43056548fa3b59d868
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-17 15:54:18 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/ParenthesisChecker.java
+++ src/final450DSA/strings/ParenthesisChecker.java
@@ -5,6 +5,24 @@
  *   File: ParenthesisChecker
  */
 
+/**
+ * GFG & LEETCODE QUESTION =>
+ * <p>
+ * Given an expression string x. Examine whether the pairs and the orders of “{“,”}”,”(“,”)”,”[“,”]” are correct in exp.
+ * For example, the function should return 'true' for exp = “[()]{}{[()()]()}” and 'false' for exp = “[(])”.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * {([])}
+ * Output:
+ * true
+ * Explanation:
+ * { ( [ ] ) }. Same colored brackets can form
+ * balanced pairs, with 0 number of
+ * unbalanced bracket.
+ */
+
 package final450DSA.strings;
 
 public class ParenthesisChecker {

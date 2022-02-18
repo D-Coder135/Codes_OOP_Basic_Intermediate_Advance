e70b9a8a57b2713e01fcfae8882bcd8bf207f200
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-18 19:38:11 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/WordBreak.java
+++ src/final450DSA/strings/WordBreak.java
@@ -5,6 +5,33 @@
  *   File: WordBreak
  */
 
+/**
+ * GFG & LEETCODE QUESTION 139 =>
+ * <p>
+ * Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated
+ * sequence of one or more dictionary words.
+ * <p>
+ * Note that the same word in the dictionary may be reused multiple times in the segmentation.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "leetcode", wordDict = ["leet","code"]
+ * Output: true
+ * Explanation: Return true because "leetcode" can be segmented as "leet code".
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: s = "applepenapple", wordDict = ["apple","pen"]
+ * Output: true
+ * Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+ * Note that you are allowed to reuse a dictionary word.
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+ * Output: false
+ */
+
 package final450DSA.strings;
 
 import java.util.ArrayList;

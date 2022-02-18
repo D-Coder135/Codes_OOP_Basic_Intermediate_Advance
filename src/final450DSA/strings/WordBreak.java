5f3b8e9543abfe279a26d788d3aa9c358c3f9220
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-18 19:34:52 +0530

    add some fix up to the code

--- src/final450DSA/strings/WordBreak.java
+++ src/final450DSA/strings/WordBreak.java
@@ -34,12 +34,14 @@ public class WordBreak {
             if (map.containsKey(s)) {
                 return map.get(s);
             }
-            for (int i = 0; i < s.length(); i++) {
+            for (int i = 1; i <= s.length(); i++) {
                 String sub = s.substring(0, i);
-                if (wordDict.contains(sub) && wordBreakDP(s.substring(i), wordDict))
+                if (wordDict.contains(sub) && wordBreakDP(s.substring(i), wordDict)) {
                     map.put(s, true);
                     return true;
+                }
             }
+            map.put(s, false);
             return false;
         }
     }

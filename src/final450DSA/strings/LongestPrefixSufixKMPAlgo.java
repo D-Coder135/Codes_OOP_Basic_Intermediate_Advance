e30dfde732449169aa7f3b370aeb77b0c83035d9
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-19 13:20:27 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/LongestPrefixSufixKMPAlgo.java
+++ src/final450DSA/strings/LongestPrefixSufixKMPAlgo.java
@@ -5,6 +5,27 @@
  *   File: LongestPrefixSufixKMPAlgo
  */
 
+/**
+ * GFG & LEETCODE QUESTION 1392 =>
+ * <p>
+ * A string is called a happy prefix if is a non-empty prefix which is also a suffix (excluding itself).
+ * <p>
+ * Given a string s, return the longest happy prefix of s. Return an empty string "" if no such prefix exists.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "level"
+ * Output: "l"
+ * Explanation: s contains 4 prefix excluding itself ("l", "le", "lev", "leve"), and suffix ("l", "el", "vel", "evel").
+ * The largest prefix which is also suffix is given by "l".
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: s = "ababab"
+ * Output: "abab"
+ * Explanation: "abab" is the largest prefix which is also suffix. They can overlap in the original string.
+ */
+
 package final450DSA.strings;
 
 public class LongestPrefixSufixKMPAlgo {

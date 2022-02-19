30fc60561af6d6848a937e82968234b88b1a01ea
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-19 11:08:39 +0530

    add a static inner class Solution inside the class LongestPrefixSufixKMPAlgo.java

---
+++ src/final450DSA/strings/LongestPrefixSufixKMPAlgo.java
@@ -0,0 +1,13 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 19-02-2022/02/2022
+ *   Time: 11:07 AM
+ *   File: LongestPrefixSufixKMPAlgo
+ */
+
+package final450DSA.strings;
+
+public class LongestPrefixSufixKMPAlgo {
+    static class Solution {
+    }
+}

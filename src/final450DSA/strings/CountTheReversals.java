51636be994bfaf75730532afc6746f0c56869148
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-19 20:55:05 +0530

    increment the value for an int field

--- src/final450DSA/strings/CountTheReversals.java
+++ src/final450DSA/strings/CountTheReversals.java
@@ -30,7 +30,7 @@ public class CountTheReversals {
                         stack.pop();
                         openingCount--;
                     } else {
-
+                        closingCount++;
                     }
                 }
             }

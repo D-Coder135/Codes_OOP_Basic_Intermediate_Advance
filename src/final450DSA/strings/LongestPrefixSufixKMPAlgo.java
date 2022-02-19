c0a87b5553cf2a826c8a9f70ca1165ebbda165a9
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-19 13:19:28 +0530

    add some code inside the method longestPrefix() along with a return statement

--- src/final450DSA/strings/LongestPrefixSufixKMPAlgo.java
+++ src/final450DSA/strings/LongestPrefixSufixKMPAlgo.java
@@ -10,7 +10,25 @@ package final450DSA.strings;
 public class LongestPrefixSufixKMPAlgo {
     static class Solution {
         public String longestPrefix(String s) {
+            int len = s.length();
+            int[] arr = new int[len];
 
+            int i = 0, j = 1;
+            while (j < len) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    arr[j] = i + 1;
+                    i++;
+                    j++;
+                } else {
+                    if (i == 0) {
+                        arr[j] = 0;
+                        j++;
+                    } else {
+                        i = arr[i - 1];
+                    }
+                }
+            }
+            return s.substring(0, arr[len - 1]);
         }
     }
 }

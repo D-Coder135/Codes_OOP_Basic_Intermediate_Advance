f50200438e020161b361ed07dec8e5bd155c5a7f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-20 21:51:04 +0530

    add an if() block

--- src/final450DSA/strings/WordSearch.java
+++ src/final450DSA/strings/WordSearch.java
@@ -12,7 +12,9 @@ public class WordSearch {
         public boolean exists(char[][] board, String word) {
             for (int i = 0; i < board.length; i++) {
                 for (int j = 0; j < board[0].length; j++) {
-                    
+                    if (board[i][j] == word.charAt(0) && dfs(board, i, j, 0, word)) {
+                        return true;
+                    }
                 }
             }
         }

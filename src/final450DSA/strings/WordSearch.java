f5a06e7926d964fb126475581d070c77b0c31b6c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-20 21:56:59 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/WordSearch.java
+++ src/final450DSA/strings/WordSearch.java
@@ -5,6 +5,25 @@
  *   File: WordSearch
  */
 
+/**
+ * LEETCODE QUESTION 79 =>
+ * <p>
+ * Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+ * <p>
+ * The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or
+ * vertically neighboring. The same letter cell may not be used more than once.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+ * Output: true
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+ * Output: true
+ */
+
 package final450DSA.strings;
 
 public class WordSearch {

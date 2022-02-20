6fb10bf6fbd9df78e881077a9e96a001b89fbdf2
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-20 21:49:44 +0530

    add nested for loops inside the method exists of type boolean

--- src/final450DSA/strings/WordSearch.java
+++ src/final450DSA/strings/WordSearch.java
@@ -9,6 +9,12 @@ package final450DSA.strings;
 
 public class WordSearch {
     static class Solution {
-
+        public boolean exists(char[][] board, String word) {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    
+                }
+            }
+        }
     }
 }

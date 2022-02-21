60daf7a17ddf056485321878f3fb431b5a3085db
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-21 19:36:38 +0530

    add an if() block along with the base condition inside it

--- src/final450DSA/strings/WordPattern.java
+++ src/final450DSA/strings/WordPattern.java
@@ -11,6 +11,10 @@ public class WordPattern {
     static class Solution {
         public boolean wordPattern(String pattern, String s) {
             String[] words = s.split(" ");
+
+            if (words.length != pattern.length()) {
+                return false;
+            }
         }
     }
 }

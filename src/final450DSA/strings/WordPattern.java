041b6f0191f907823a11bc9d9ab4ddb0619b37c1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-21 19:36:13 +0530

    initialize a an array object of type String

--- src/final450DSA/strings/WordPattern.java
+++ src/final450DSA/strings/WordPattern.java
@@ -10,7 +10,7 @@ package final450DSA.strings;
 public class WordPattern {
     static class Solution {
         public boolean wordPattern(String pattern, String s) {
-
+            String[] words = s.split(" ");
         }
     }
 }

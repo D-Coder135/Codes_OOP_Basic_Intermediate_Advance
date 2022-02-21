222322dc4a572d34b5c49e63fa4d7056faa13b93
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-21 19:34:41 +0530

    add a new class WordPattern.java inside the package final450DSA.strings

---
+++ src/final450DSA/strings/WordPattern.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 21-02-2022/02/2022
+ *   Time: 07:34 PM
+ *   File: WordPattern
+ */
+
+package final450DSA.strings;
+
+public class WordPattern {
+}

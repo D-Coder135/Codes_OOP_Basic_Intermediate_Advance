937cc5f7348c952d125b5e0e388c276d83e815eb
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-22 18:46:40 +0530

    add an object of type HashMap<String, Integer>

--- src/final450DSA/strings/SecondFrequent.java
+++ src/final450DSA/strings/SecondFrequent.java
@@ -7,10 +7,14 @@
 
 package final450DSA.strings;
 
+import java.util.HashMap;
+
 public class SecondFrequent {
     static class Solution {
         String secFrequent(String[] arr, int N) {
             // your code here
+            HashMap<String, Integer> map = new HashMap<>();
+
 
         }
     }

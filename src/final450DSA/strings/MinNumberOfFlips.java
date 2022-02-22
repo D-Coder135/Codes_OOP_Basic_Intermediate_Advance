02208306977bf3478ee418af2932969eb5ac9a5a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-22 16:21:46 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/MinNumberOfFlips.java
+++ src/final450DSA/strings/MinNumberOfFlips.java
@@ -5,6 +5,31 @@
  *   File: MinNumberOfFlips
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * Given a binary string, that is it contains only 0s and 1s. We need to make this string a sequence of alternate
+ * characters by flipping some bits, our goal is to minimize the number of bits to be flipped.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * S = "001"
+ * Output: 1
+ * Explanation:
+ * We can flip the 0th bit to 1 to have
+ * 101.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input:
+ * S = "0001010111"
+ * Output: 2
+ * Explanation: We can flip the 1st and 8th bit
+ * bit to have "0101010101"
+ * 101.
+ */
+
 package final450DSA.strings;
 
 public class MinNumberOfFlips {

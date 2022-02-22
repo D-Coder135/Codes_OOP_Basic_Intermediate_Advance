b0c4fad66ef879a8fc579054c59ef5f552e42236
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-22 14:46:15 +0530

    add a new class LongestCommonPrefix.java inside the package final450DSA.strings

---
+++ src/final450DSA/strings/LongestCommonPrefix.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 22-02-2022/02/2022
+ *   Time: 02:45 PM
+ *   File: LongestCommonPrefix
+ */
+
+package final450DSA.strings;
+
+public class LongestCommonPrefix {
+}

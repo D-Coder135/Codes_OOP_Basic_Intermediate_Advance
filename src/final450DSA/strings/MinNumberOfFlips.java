12e7a82dfc29f3b4e0a0bc8f8d214dc845d89d10
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-22 16:17:21 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/MinNumberOfFlips.java
+++ src/final450DSA/strings/MinNumberOfFlips.java
@@ -5,6 +5,33 @@
  *   File: MinNumberOfFlips
  */
 
+/**
+ * LEETCODE QUESTION 1888 =>
+ * <p>
+ * You are given a binary string s. You are allowed to perform two types of operations on the string in any sequence:
+ * <p>
+ * Type-1: Remove the character at the start of the string s and append it to the end of the string.
+ * Type-2: Pick any character in s and flip its value, i.e., if its value is '0' it becomes '1' and vice-versa.
+ * Return the minimum number of type-2 operations you need to perform such that s becomes alternating.
+ * <p>
+ * The string is called alternating if no two adjacent characters are equal.
+ * <p>
+ * For example, the strings "010" and "1010" are alternating, while the string "0100" is not.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "111000"
+ * Output: 2
+ * Explanation: Use the first operation two times to make s = "100011".
+ * Then, use the second operation on the third and sixth elements to make s = "101010".
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: s = "010"
+ * Output: 0
+ * Explanation: The string is already alternating.
+ */
+
 package final450DSA.strings;
 
 public class MinNumberOfFlips {

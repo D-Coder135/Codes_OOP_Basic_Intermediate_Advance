7e596432f070c2d6c80c279c37ae069764550670
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-22 14:49:40 +0530

    call the substring() method on prefix String field

--- src/final450DSA/strings/LongestCommonPrefix.java
+++ src/final450DSA/strings/LongestCommonPrefix.java
@@ -18,7 +18,7 @@ public class LongestCommonPrefix {
 
             for (int i = 1; i < strs.length; i++) {
                 while (strs[i].indexOf(prefix) != 0) {
-
+                    prefix = prefix.substring(0, prefix.length() - 1);
                 }
             }
         }

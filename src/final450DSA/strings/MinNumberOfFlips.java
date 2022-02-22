8538487603099e07a27260ffc8bdf451722539f3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-22 16:20:35 +0530

    add an if() block inside the for loop

--- src/final450DSA/strings/MinNumberOfFlips.java
+++ src/final450DSA/strings/MinNumberOfFlips.java
@@ -13,7 +13,9 @@ public class MinNumberOfFlips {
             int first = 0, second = 0;
 
             for (int i = 0; i < S.length(); i++) {
-
+                if ((i % 2 == 0 && S.charAt(i) == '1') || (i % 2 != 0 && S.charAt(i) == '0')) {
+                    first++;
+                }
             }
         }
     }

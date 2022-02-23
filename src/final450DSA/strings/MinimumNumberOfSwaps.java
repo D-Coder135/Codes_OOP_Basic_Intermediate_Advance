b74e17c0dd1e9b8c7cf8a57853dcc8f2f8e1ae4d
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-23 21:08:55 +0530

    increment the value for open field

--- src/final450DSA/strings/MinimumNumberOfSwaps.java
+++ src/final450DSA/strings/MinimumNumberOfSwaps.java
@@ -16,7 +16,7 @@ public class MinimumNumberOfSwaps {
                     close++;
                     fault = close - open;
                 } else {
-
+                    open++;
                 }
             }
         }

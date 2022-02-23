5a5112cddf0140eff09742e4ebef340d442393c1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-23 21:09:07 +0530

    add an if() block

--- src/final450DSA/strings/MinimumNumberOfSwaps.java
+++ src/final450DSA/strings/MinimumNumberOfSwaps.java
@@ -17,6 +17,9 @@ public class MinimumNumberOfSwaps {
                     fault = close - open;
                 } else {
                     open++;
+                    if (fault > 0) {
+
+                    }
                 }
             }
         }

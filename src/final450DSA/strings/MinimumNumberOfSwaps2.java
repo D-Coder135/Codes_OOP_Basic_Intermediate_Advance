624070039492d172628a89552d23295c98bc0e7e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-23 21:18:36 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/MinimumNumberOfSwaps2.java
+++ src/final450DSA/strings/MinimumNumberOfSwaps2.java
@@ -5,6 +5,39 @@
  *   File: MinimumNumberOfSwaps2
  */
 
+/**
+ * LEETCODE QUESTION 1963 =>
+ * <p>
+ * You are given a 0-indexed string s of even length n. The string consists of exactly n / 2 opening brackets '[' and
+ * n / 2 closing brackets ']'.
+ * <p>
+ * A string is called balanced if and only if:
+ * <p>
+ * It is the empty string, or
+ * It can be written as AB, where both A and B are balanced strings, or
+ * It can be written as [C], where C is a balanced string.
+ * You may swap the brackets at any two indices any number of times.
+ * <p>
+ * Return the minimum number of swaps to make s balanced.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "][]["
+ * Output: 1
+ * Explanation: You can make the string balanced by swapping index 0 with index 3.
+ * The resulting string is "[[]]".
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: s = "]]][[["
+ * Output: 2
+ * Explanation: You can do the following to make the string balanced:
+ * - Swap index 0 with index 4. s = "[]][][".
+ * - Swap index 1 with index 5. s = "[[][]]".
+ * The resulting string is "[[][]]".
+ */
+
 package final450DSA.strings;
 
 import java.util.Stack;

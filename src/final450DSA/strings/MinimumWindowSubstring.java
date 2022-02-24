d78adea01cfae2a126fac6bf979e1d4b2a47fef5
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-24 22:28:33 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/MinimumWindowSubstring.java
+++ src/final450DSA/strings/MinimumWindowSubstring.java
@@ -5,6 +5,31 @@
  *   File: MinimumWindowSubstring
  */
 
+/**
+ * LEETCODE QUESTION 76 =>
+ * <p>
+ * Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that
+ * every character in t (including duplicates) is included in the window. If there is no such substring,
+ * return the empty string "".
+ * <p>
+ * The testcases will be generated such that the answer is unique.
+ * <p>
+ * A substring is a contiguous sequence of characters within the string.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "ADOBECODEBANC", t = "ABC"
+ * Output: "BANC"
+ * Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: s = "a", t = "a"
+ * Output: "a"
+ * Explanation: The entire string s is the minimum window.
+ */
+
 package final450DSA.strings;
 
 import java.util.HashMap;

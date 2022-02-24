c5e5554f2acabcd5ab6b94737ee4ed2c2da63d0f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-24 22:25:26 +0530

    update the value for the field flag2

--- src/final450DSA/strings/MinimumWindowSubstring.java
+++ src/final450DSA/strings/MinimumWindowSubstring.java
@@ -60,6 +60,7 @@ public class MinimumWindowSubstring {
                     if (map1.getOrDefault(ch, 0) < map2.getOrDefault(ch, 0)) {
                         matchCount--;
                     }
+                    flag2 = true;
                 }
             }
         }

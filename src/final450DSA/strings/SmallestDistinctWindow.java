4b540fe185159a2796c2a55d775e0c0a8707fc9b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-24 21:48:54 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/SmallestDistinctWindow.java
+++ src/final450DSA/strings/SmallestDistinctWindow.java
@@ -5,6 +5,25 @@
  *   File: SmallestDistinctWindow
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * Given a string 's'. The task is to find the smallest window length that contains all the characters of the given
+ * string at least one time.
+ * For eg. A = aabcbcdbca, then the result would be 4 as of the smallest window will be dbca.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input : "AABBBCBBAC"
+ * Output : 3
+ * Explanation : Sub-string -> "BAC"
+ * <p>
+ * Example 2:
+ * Input : "aaab"
+ * Output : 2
+ * Explanation : Sub-string -> "ab"
+ */
+
 package final450DSA.strings;
 
 import java.util.HashSet;

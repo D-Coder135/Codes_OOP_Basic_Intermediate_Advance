3f9d1579f43eccb803d4341cc34aec546269c99c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-26 19:46:35 +0530

    add a new class RemoveConsecutiveCharacters.java inside the package final450DSA.strings

---
+++ src/final450DSA/strings/RemoveConsecutiveCharacters.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 26-02-2022/02/2022
+ *   Time: 07:46 PM
+ *   File: RemoveConsecutiveCharacters
+ */
+
+package final450DSA.strings;
+
+public class RemoveConsecutiveCharacters {
+}

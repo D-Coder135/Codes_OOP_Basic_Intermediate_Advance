de94120364257826751e1a428f1a17a109176a17
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-26 10:35:40 +0530

    add some fix up to the code

--- src/final450DSA/strings/LongestPalindromicSubsequence.java
+++ src/final450DSA/strings/LongestPalindromicSubsequence.java
@@ -21,7 +21,7 @@ public class LongestPalindromicSubsequence {
                         if (s.charAt(i) == s.charAt(j)) {
                             dp[i][j] = 2 + dp[i + 1][j - 1];
                         } else {
-                            dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                            dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
                         }
                     }
                 }

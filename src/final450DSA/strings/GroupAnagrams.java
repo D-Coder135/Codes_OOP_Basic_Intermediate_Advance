62195fb5c69b7b3d7c95bb8991a938f234635e35
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-26 14:27:53 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/GroupAnagrams.java
+++ src/final450DSA/strings/GroupAnagrams.java
@@ -5,6 +5,26 @@
  *   File: GroupAnagrams
  */
 
+/**
+ * GFG & LEETCODE QUESTION 49 =>
+ * <p>
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ * <p>
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using
+ * all the original letters exactly once.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: strs = [""]
+ * Output: [[""]]
+ */
+
 package final450DSA.strings;
 
 import java.util.*;

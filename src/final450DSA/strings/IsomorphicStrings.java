2a5d32937e167c5ad913519149c62cc4948ad76e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-27 20:00:00 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/strings/IsomorphicStrings.java
+++ src/final450DSA/strings/IsomorphicStrings.java
@@ -5,6 +5,28 @@
  *   File: IsomorphicStrings
  */
 
+/**
+ * GFG & LEETCODE QUESTION 205 =>
+ * <p>
+ * Given two strings s and t, determine if they are isomorphic.
+ * <p>
+ * Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+ * <p>
+ * All occurrences of a character must be replaced with another character while preserving the order of characters.
+ * No two characters may map to the same character, but a character may map to itself.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "egg", t = "add"
+ * Output: true
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: s = "foo", t = "bar"
+ * Output: false
+ */
+
 package final450DSA.strings;
 
 import java.util.HashMap;

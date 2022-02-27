f7cb490243c2fcac417e143f14359db06cf617d6
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-27 19:46:35 +0530

    initialize a char field

--- src/final450DSA/strings/IsomorphicStrings.java
+++ src/final450DSA/strings/IsomorphicStrings.java
@@ -18,7 +18,7 @@ public class IsomorphicStrings {
         private String helper(String s) {
             StringBuilder ans = new StringBuilder();
             for (int i = 0; i < s.length(); i++) {
-
+                char ch = s.charAt(i);
             }
             return ans.toString();
         }

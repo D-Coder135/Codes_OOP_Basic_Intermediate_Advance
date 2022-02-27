ee04bdf1227615a4d40dedb67fbe95e57f2b0fad
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-27 22:41:36 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/searchingAndSorting/FirstAndLastOccurrences.java
+++ src/final450DSA/searchingAndSorting/FirstAndLastOccurrences.java
@@ -5,6 +5,28 @@
  *   File: FirstAndLastOccurrences
  */
 
+/**
+ * GFG & LEETCODE QUESTION 34 =>
+ * <p>
+ * Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a
+ * given target value.
+ * <p>
+ * If target is not found in the array, return [-1, -1].
+ * <p>
+ * You must write an algorithm with O(log n) runtime complexity.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ */
+
 package final450DSA.searchingAndSorting;
 
 public class FirstAndLastOccurrences {

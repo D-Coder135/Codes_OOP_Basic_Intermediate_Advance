ffc7e96d7c64987f0f20b7165f497a6e8872c255
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-27 18:01:14 +0530

    add an if() block

--- src/final450DSA/strings/WildcardMatching.java
+++ src/final450DSA/strings/WildcardMatching.java
@@ -13,7 +13,9 @@ public class WildcardMatching {
             boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
             for (int i = dp.length - 1; i >= 0; i--) {
                 for (int j = dp[0].length - 1; j >= 0; j--) {
-
+                    if (i == dp.length - 1 && j == dp[0].length - 1) {
+                        dp[i][j] = true;
+                    }
                 }
             }
         }

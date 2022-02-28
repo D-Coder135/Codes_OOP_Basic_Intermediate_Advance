b2e8e0f6e40aff64ab8b68058539ac2d9d84dd0c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-28 22:03:09 +0530

    add an if() block along with a return statement

--- src/final450DSA/searchingAndSorting/MissingNumber.java
+++ src/final450DSA/searchingAndSorting/MissingNumber.java
@@ -18,7 +18,9 @@ public class MissingNumber {
             }
 
             for (int i = 0; i <= nums.length; i++) {
-
+                if (!set.contains(i)) {
+                    return i;
+                }
             }
         }
     }

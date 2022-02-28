856b7b125e099b8225e50e6f39035adaf11973b2
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-28 13:28:23 +0530

    update the value for the field totalSum

--- src/final450DSA/searchingAndSorting/MiddleIndexInArray.java
+++ src/final450DSA/searchingAndSorting/MiddleIndexInArray.java
@@ -21,7 +21,7 @@ public class MiddleIndexInArray {
 
             int leftSum = 0;
             for (int i = 0; i < nums.length; i++) {
-
+                totalSum -= nums[i];
             }
         }
     }

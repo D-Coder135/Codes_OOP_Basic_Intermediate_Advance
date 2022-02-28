bdd124ad734b1fce68a017359e77040a4a11754b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-28 11:11:54 +0530

    add the problem statement as comment inside the code

--- src/leetCodes/PerfectSquares.java
+++ src/leetCodes/PerfectSquares.java
@@ -5,6 +5,28 @@
  *   File: PerfectSquares
  */
 
+/**
+ * LEETCODE QUESTION 279 =>
+ * <p>
+ * Given an integer n, return the least number of perfect square numbers that sum to n.
+ * <p>
+ * A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer
+ * with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 12
+ * Output: 3
+ * Explanation: 12 = 4 + 4 + 4.
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: n = 13
+ * Output: 2
+ * Explanation: 13 = 4 + 9.
+ */
+
 package leetCodes;
 
 public class PerfectSquares {

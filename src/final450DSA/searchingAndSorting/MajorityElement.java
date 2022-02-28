0def191fac9b993032b2b5e40d164eb33477798f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-01 00:06:28 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/searchingAndSorting/MajorityElement.java
+++ src/final450DSA/searchingAndSorting/MajorityElement.java
@@ -5,6 +5,26 @@
  *   File: MajorityElements
  */
 
+/**
+ * GFG & LEETCODE QUESTION 169 =>
+ * <p>
+ * Given an array nums of size n, return the majority element.
+ * <p>
+ * The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the
+ * majority element always exists in the array.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [3,2,3]
+ * Output: 3
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [2,2,1,1,1,2,2]
+ * Output: 2
+ */
+
 package final450DSA.searchingAndSorting;
 
 import java.util.HashMap;

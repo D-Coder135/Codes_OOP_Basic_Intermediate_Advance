875ff1b18587162994ce68e9458ad254926892bb
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-28 21:58:34 +0530

    add a return statement

--- src/final450DSA/searchingAndSorting/RepeatingAndMissingElement.java
+++ src/final450DSA/searchingAndSorting/RepeatingAndMissingElement.java
@@ -24,6 +24,7 @@ public class RepeatingAndMissingElement {
                     missing = i + 1;
                 }
             }
+            return new int[]{repeating, missing};
         }
     }
 }

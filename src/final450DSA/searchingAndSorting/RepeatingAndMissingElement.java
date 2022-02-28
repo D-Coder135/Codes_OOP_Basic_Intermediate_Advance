7e92759a6b318591b4a5f2db0ffcbc02ca8a277f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-28 21:55:59 +0530

    add an if() block along with a condition inside it

--- src/final450DSA/searchingAndSorting/RepeatingAndMissingElement.java
+++ src/final450DSA/searchingAndSorting/RepeatingAndMissingElement.java
@@ -12,7 +12,9 @@ public class RepeatingAndMissingElement {
         int[] findTwoElement(int[] arr, int n) {
             int repeating = 0, missing = 0;
             for (int i = 0; i < n; i++) {
+                if (arr[Math.abs(arr[i]) - 1] < 0) {
 
+                }
             }
         }
     }

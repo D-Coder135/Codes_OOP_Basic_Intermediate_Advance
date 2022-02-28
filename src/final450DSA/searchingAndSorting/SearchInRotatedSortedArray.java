9c5e631092cbcf0b64f5f352bbd62dc67137735d
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-28 10:34:18 +0530

    add an else block

--- src/final450DSA/searchingAndSorting/SearchInRotatedSortedArray.java
+++ src/final450DSA/searchingAndSorting/SearchInRotatedSortedArray.java
@@ -17,7 +17,9 @@ public class SearchInRotatedSortedArray {
                     return mid;
                 } else if (nums[0] < nums[mid]) {
                     if (target > nums[0] && target < nums[mid]) {
-
+                        end = mid - 1;
+                    } else {
+                        start = mid + 1;
                     }
                 }
             }

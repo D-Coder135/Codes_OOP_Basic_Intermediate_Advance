975a84a22d8f90074d692edad198ff8ffc598e7f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-28 10:40:58 +0530

    add some fix up to the code

--- src/final450DSA/searchingAndSorting/SearchInRotatedSortedArray.java
+++ src/final450DSA/searchingAndSorting/SearchInRotatedSortedArray.java
@@ -10,19 +10,19 @@ package final450DSA.searchingAndSorting;
 public class SearchInRotatedSortedArray {
     static class Solution {
         public int search(int[] nums, int target) {
-            int start = 0, end = nums.length;
+            int start = 0, end = nums.length - 1;
             while (start <= end) {
                 int mid = (start + end) / 2;
                 if (nums[mid] == target) {
                     return mid;
-                } else if (nums[0] <= nums[mid]) {
-                    if (target > nums[0] && target < nums[mid]) {
+                } else if (nums[start] <= nums[mid]) {
+                    if (target >= nums[start] && target <= nums[mid]) {
                         end = mid - 1;
                     } else {
                         start = mid + 1;
                     }
                 } else {
-                    if (target >= nums[mid] && target < nums[end]) {
+                    if (target >= nums[mid] && target <= nums[end]) {
                         start = mid + 1;
                     } else {
                         end = mid - 1;

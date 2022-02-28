e346fe9320a553cb1fb2ca4523b55c99a2185890
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-28 11:09:34 +0530

    initialize an int field

--- src/leetCodes/PerfectSquares.java
+++ src/leetCodes/PerfectSquares.java
@@ -17,7 +17,7 @@ public class PerfectSquares {
             for (int i = 2; i <= n; i++) {
                 int min = Integer.MAX_VALUE;
                 for (int j = 1; j * j <= i; j++) {
-
+                    int rem = i - (j * j);
                 }
             }
         }

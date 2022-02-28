22c09774a60c8b1b101f78e379ad1dbed3451346
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-28 11:05:38 +0530

    add a new class PerfectSquares.java inside the package leetCodes

---
+++ src/leetCodes/PerfectSquares.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 28-02-2022/02/2022
+ *   Time: 11:05 AM
+ *   File: PerfectSquares
+ */
+
+package leetCodes;
+
+public class PerfectSquares {
+}

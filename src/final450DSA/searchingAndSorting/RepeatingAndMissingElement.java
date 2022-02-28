fa61437a2387b92a48b776c6b8d72831113ff483
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-28 21:59:27 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/searchingAndSorting/RepeatingAndMissingElement.java
+++ src/final450DSA/searchingAndSorting/RepeatingAndMissingElement.java
@@ -5,6 +5,32 @@
  *   File: RepeatingAndMissingElement
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * Given an unsorted array Arr of size N of positive integers. One number 'A' from set {1, 2, …N} is missing
+ * and one number 'B' occurs twice in array. Find these two numbers.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * N = 2
+ * Arr[] = {2, 2}
+ * Output: 2 1
+ * Explanation: Repeating number is 2 and
+ * smallest positive missing number is 1.
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input:
+ * N = 3
+ * Arr[] = {1, 3, 3}
+ * Output: 3 2
+ * Explanation: Repeating number is 3 and
+ * smallest positive missing number is 2.
+ */
+
 package final450DSA.searchingAndSorting;
 
 public class RepeatingAndMissingElement {

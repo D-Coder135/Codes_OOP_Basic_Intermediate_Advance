b3f8e1a8260b3b4e13fa9a0ec1b542b0702b1370
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-01 00:00:58 +0530

    add a new class MajorityElement.java inside the package final450DSA.searchingAndSorting

---
+++ src/final450DSA/searchingAndSorting/MajorityElement.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 01-03-2022/03/2022
+ *   Time: 12:00 AM
+ *   File: MajorityElements
+ */
+
+package final450DSA.searchingAndSorting;
+
+public class MajorityElement {
+}

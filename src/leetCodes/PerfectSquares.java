699df219e02e19a4c75597cdc8044485c70f318e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-28 11:08:27 +0530

    add a for loop

--- src/leetCodes/PerfectSquares.java
+++ src/leetCodes/PerfectSquares.java
@@ -13,6 +13,10 @@ public class PerfectSquares {
             int[] dp = new int[n + 1];
             dp[0] = 0;
             dp[1] = 1;
+
+            for (int i = 2; i <= n; i++) {
+
+            }
         }
     }
 }

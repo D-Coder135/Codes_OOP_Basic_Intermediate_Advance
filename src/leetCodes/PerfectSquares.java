e805c68e5b9b6f58829dc9da7dbf583cc9e5ac51
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-02-28 11:08:59 +0530

    add a for loop inside the for loop

--- src/leetCodes/PerfectSquares.java
+++ src/leetCodes/PerfectSquares.java
@@ -15,7 +15,9 @@ public class PerfectSquares {
             dp[1] = 1;
 
             for (int i = 2; i <= n; i++) {
+                for (int j = 1; j * j <= i; j++) {
 
+                }
             }
         }
     }

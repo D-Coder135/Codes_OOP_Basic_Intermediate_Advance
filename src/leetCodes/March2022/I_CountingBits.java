f05d7bcf98b47a70c67142c88eb3a3f2f85f7be1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-01 14:26:38 +0530

    add a for loop

--- src/leetCodes/March2022/I_CountingBits.java
+++ src/leetCodes/March2022/I_CountingBits.java
@@ -12,6 +12,10 @@ public class I_CountingBits {
         public int[] countBits(int n) {
             int[] ans = new int[n + 1];
             ans[0] = 0;
+
+            for (int i = 1; i <= n; i++) {
+                ans[i] = ans[i / 2] + i % 2;
+            }
             return ans;
         }
     }

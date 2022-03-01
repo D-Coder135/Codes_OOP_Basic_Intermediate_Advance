1eb391e0c3f15ec9b0faf068c1101a10c4b13fe3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-01 10:39:17 +0530

    add a new class CountNumberOfPairsWithDifferenceK.java inside the package final450DSA.searchingAndSorting

---
+++ src/final450DSA/searchingAndSorting/CountNumberOfPairsWithDifferenceK.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 01-03-2022/03/2022
+ *   Time: 10:38 AM
+ *   File: CountNumberOfPairsWithDifferenceK
+ */
+
+package final450DSA.searchingAndSorting;
+
+public class CountNumberOfPairsWithDifferenceK {
+}

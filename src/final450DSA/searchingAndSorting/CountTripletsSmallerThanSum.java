295ffff307976aaf494bc473dba9c737be42466b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-02 23:51:03 +0530

    add a new class CountTripletsSmallerThanSum.java inside the package final450DSA.searchingAndSorting

---
+++ src/final450DSA/searchingAndSorting/CountTripletsSmallerThanSum.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 02-03-2022/03/2022
+ *   Time: 11:50 PM
+ *   File: CountTripletsSmallerThanSum
+ */
+
+package final450DSA.searchingAndSorting;
+
+public class CountTripletsSmallerThanSum {
+}
--- src/leetCodes/HouseRobber2.java
+++ src/leetCodes/HouseRobber2.java
@@ -7,12 +7,25 @@
 
 package leetCodes;
 
+import java.util.Arrays;
+
 public class HouseRobber2 {
     static class Solution {
         public int rob(int[] nums) {
             if (nums.length == 1) {
                 return nums[0];
             }
+            return Math.max(helper(nums, 0, nums.length - 1), helper(nums, 1, nums.length));
+        }
+
+        private int helper(int[] nums, int start, int end) {
+            int max = 0, prevTwo = 0, prevOne = 0;
+            for (int i = start; i < end; i++) {
+                max = Math.max(prevTwo + nums[i], prevOne);
+                prevTwo = prevOne;
+                prevOne = max;
+            }
+            return max;
         }
     }
 }

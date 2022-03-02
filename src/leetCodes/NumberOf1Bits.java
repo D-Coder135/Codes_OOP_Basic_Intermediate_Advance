236b90f77ac409977cf90a485c3471fe9dbc8fab
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-03 00:23:32 +0530

    add another approach as comment inside the code

--- src/leetCodes/NumberOf1Bits.java
+++ src/leetCodes/NumberOf1Bits.java
@@ -20,4 +20,20 @@ public class NumberOf1Bits {
             return counter;
         }
     }
+
+    /*
+    * ANOTHER APPROACH
+    *
+    * public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int counter = 0, mask = 1;
+        for(int i = 0; i < 32; i++) {
+            if((n & mask) != 0) counter++;
+            mask = mask << 1;
+        }
+        return counter;
+    }
+}
+    * */
 }

d7f4843c1049318ebc4f2fa5c28e31b97c0c038e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-03 00:22:51 +0530

    add a static inner class Solution along with the solution code inside its body

--- src/leetCodes/NumberOf1Bits.java
+++ src/leetCodes/NumberOf1Bits.java
@@ -8,4 +8,16 @@
 package leetCodes;
 
 public class NumberOf1Bits {
+    public static class Solution {
+        // you need to treat n as an unsigned value
+        public int hammingWeight(int n) {
+            int counter = 0;
+            while (n != 0) {
+                n = n & (n - 1);
+                counter++;
+            }
+
+            return counter;
+        }
+    }
 }

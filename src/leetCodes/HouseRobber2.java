ed941d2008428fd8efb9f52759f7130e4f4a63a3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-02 18:38:36 +0530

    add a new class HouseRobber2.java inside the package leetCodes

---
+++ src/leetCodes/HouseRobber2.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 02-03-2022/03/2022
+ *   Time: 06:38 PM
+ *   File: HouseRobber2
+ */
+
+package leetCodes;
+
+public class HouseRobber2 {
+}

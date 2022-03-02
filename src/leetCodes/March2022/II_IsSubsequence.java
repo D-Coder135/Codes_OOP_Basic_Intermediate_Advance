0e1f20f535dab9aab7a11f2f201fab8cb430c9f0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-03 00:02:55 +0530

    add while loop

--- src/leetCodes/March2022/II_IsSubsequence.java
+++ src/leetCodes/March2022/II_IsSubsequence.java
@@ -11,6 +11,10 @@ public class II_IsSubsequence {
     static class Solution {
         public boolean isSubsequence(String s, String t) {
             int i = 0, j = 0;
+
+            while (i < s.length() && j < t.length()) {
+
+            }
         }
     }
 }

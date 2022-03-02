f937d7ad4e4b21c097a25e78fbb5429b5aeb017d
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-03 00:03:19 +0530

    add an if() block along with a condition inside it

--- src/leetCodes/March2022/II_IsSubsequence.java
+++ src/leetCodes/March2022/II_IsSubsequence.java
@@ -13,7 +13,10 @@ public class II_IsSubsequence {
             int i = 0, j = 0;
 
             while (i < s.length() && j < t.length()) {
-
+                if (s.charAt(i) == t.charAt(j)) {
+                    i++;
+                    j++;
+                }
             }
         }
     }

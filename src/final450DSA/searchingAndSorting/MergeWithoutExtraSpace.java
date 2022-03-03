d068067522d23e6afdb48101e641a84ad0e6102c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-03 10:10:58 +0530

    add an else block

--- src/final450DSA/searchingAndSorting/MergeWithoutExtraSpace.java
+++ src/final450DSA/searchingAndSorting/MergeWithoutExtraSpace.java
@@ -17,6 +17,12 @@ public class MergeWithoutExtraSpace {
             while (i <= k && j < m) {
                 if (arr1[i] < arr2[j]) {
                     i++;
+                } else {
+                    int temp = arr2[j];
+                    arr2[j] = arr1[k];
+                    arr1[k] = temp;
+                    j++;
+                    k--;
                 }
             }
         }

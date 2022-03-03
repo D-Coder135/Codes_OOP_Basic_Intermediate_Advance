880e538fe1a635821232f07b6ff231c4266e662e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-03 10:51:03 +0530

    add the solution code inside the method findSubarray()

--- src/final450DSA/searchingAndSorting/ZeroSumSubarrays.java
+++ src/final450DSA/searchingAndSorting/ZeroSumSubarrays.java
@@ -7,11 +7,24 @@
 
 package final450DSA.searchingAndSorting;
 
+import java.util.HashMap;
+
 public class ZeroSumSubarrays {
     static class Solution {
         //Function to count subarrays with sum equal to 0.
-        public static long findSubarray(long[] arr, int n) {
+        public static long findSubarray(long[] nums, int n) {
             //Your code here
+            HashMap<Integer, Integer> map = new HashMap<>();
+            int sum = 0, ans = 0;
+            map.put(0, 1);
+            for (long num : nums) {
+                sum += num;
+                if (map.containsKey(sum)) {
+                    ans += map.get(sum);
+                }
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+            return ans;
         }
     }
 }

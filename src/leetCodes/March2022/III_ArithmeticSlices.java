c41512df6f67634e6c1599c359bb46da19b09e43
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-03 09:48:54 +0530

    add the problem statement as comment inside the code

--- src/leetCodes/March2022/III_ArithmeticSlices.java
+++ src/leetCodes/March2022/III_ArithmeticSlices.java
@@ -5,6 +5,30 @@
  *   File: III_ArithmeticSlices
  */
 
+/**
+ * LEETCODE QUESTION 413 =>
+ * <p>
+ * An integer array is called arithmetic if it consists of at least three elements and if the
+ * difference between any two consecutive elements is the same.
+ * <p>
+ * For example, [1,3,5,7,9], [7,7,7,7], and [3,-1,-5,-9] are arithmetic sequences.
+ * Given an integer array nums, return the number of arithmetic sub-arrays of nums.
+ * <p>
+ * A subarray is a contiguous subsequence of the array.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,3,4]
+ * Output: 3
+ * Explanation: We have 3 arithmetic slices in nums: [1, 2, 3], [2, 3, 4] and [1,2,3,4] itself.
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [1]
+ * Output: 0
+ */
+
 package leetCodes.March2022;
 
 public class III_ArithmeticSlices {

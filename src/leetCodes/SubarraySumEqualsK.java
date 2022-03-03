1168b6551127d57c98f3a90b023ad452b156ed8d
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-03 10:39:25 +0530

    add an if() block inside the for loop

--- src/leetCodes/SubarraySumEqualsK.java
+++ src/leetCodes/SubarraySumEqualsK.java
@@ -17,6 +17,9 @@ public class SubarraySumEqualsK {
             map.put(0, 1);
             for (int i = 0; i < nums.length; i++) {
                 sum += nums[i];
+                if (map.containsKey(sum - k)) {
+                    ans += map.get(sum - k);
+                }
             }
         }
     }

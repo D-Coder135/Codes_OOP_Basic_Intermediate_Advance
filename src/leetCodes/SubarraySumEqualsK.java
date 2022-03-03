2c46c1a4855f19eb7dee722c5910472d5223116f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-03 10:38:53 +0530

    add a for loop

--- src/leetCodes/SubarraySumEqualsK.java
+++ src/leetCodes/SubarraySumEqualsK.java
@@ -15,6 +15,9 @@ public class SubarraySumEqualsK {
             HashMap<Integer, Integer> map = new HashMap<>();
             int sum = 0, ans = 0;
             map.put(0, 1);
+            for (int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+            }
         }
     }
 }

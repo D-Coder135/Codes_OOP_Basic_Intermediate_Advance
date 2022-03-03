bf3edaad618128ba14c356801cad2f9c4f3cb59a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-03 10:51:57 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/searchingAndSorting/ZeroSumSubarrays.java
+++ src/final450DSA/searchingAndSorting/ZeroSumSubarrays.java
@@ -5,6 +5,30 @@
  *   File: ZeroSumSubarrays
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * You are given an array arr[] of size n. Find the total count of sub-arrays having their sum equal to 0.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * n = 6
+ * arr[] = {0,0,5,5,0,0}
+ * Output: 6
+ * Explanation: The 6 subarrays are
+ * [0], [0], [0], [0], [0,0], and [0,0].
+ * <p>
+ * Example 2:
+ * <p>
+ * Input:
+ * n = 10
+ * arr[] = {6,-1,-3,4,-2,2,4,6,-12,-7}
+ * Output: 4
+ * Explanation: The 4 subarrays are [-1 -3 4]
+ * [-2 2], [2 4 6 -12], and [-1 -3 4 -2 2]
+ */
+
 package final450DSA.searchingAndSorting;
 
 import java.util.HashMap;

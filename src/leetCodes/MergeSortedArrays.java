2107acad5f0bd9f8705f893574531dd48392a82c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-03 10:14:54 +0530

    add a for loop

--- src/leetCodes/MergeSortedArrays.java
+++ src/leetCodes/MergeSortedArrays.java
@@ -11,7 +11,10 @@ public class MergeSortedArrays {
     static class Solution {
         public void merge(int[] nums1, int m, int[] nums2, int n) {
             int j = 0;
-
+            for (int i = m; i < m + n; i++) {
+                nums1[i] = nums2[j];
+                j++;
+            }
         }
     }
 }

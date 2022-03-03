99936499c62871eba5e26696197913b186ba244b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-03 10:41:34 +0530

    add the problem statement as comment inside the code

--- src/leetCodes/SubarraySumEqualsK.java
+++ src/leetCodes/SubarraySumEqualsK.java
@@ -5,6 +5,23 @@
  *   File: SubarraySumEqualsK
  */
 
+/**
+ * LEETCODE QUESTION 560 =>
+ * <p>
+ * Given an array of integers nums and an integer k, return the total number of sub-arrays whose sum equals to k.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,1,1], k = 2
+ * Output: 2
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [1,2,3], k = 3
+ * Output: 2
+ */
+
 package leetCodes;
 
 import java.util.HashMap;

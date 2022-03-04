d328e692b481c9d47fc583823c0d1efd557586ce
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-04 19:48:26 +0530

    add a static inner class Solution along with the solution code

--- src/final450DSA/searchingAndSorting/SortBySetBitCount.java
+++ src/final450DSA/searchingAndSorting/SortBySetBitCount.java
@@ -7,10 +7,21 @@
 
 package final450DSA.searchingAndSorting;
 
+import java.util.Arrays;
+
 public class SortBySetBitCount {
     static class Solution {
         public int[] sortByBits(int[] arr) {
 
+            int n = arr.length;
+            for (int i = 0; i < n; i++)
+                arr[i] += 10001 * Integer.bitCount(arr[i]);
+
+            Arrays.sort(arr);
+
+            for (int i = 0; i < n; i++)
+                arr[i] = arr[i] % 10001;
+            return arr;
         }
     }
 }

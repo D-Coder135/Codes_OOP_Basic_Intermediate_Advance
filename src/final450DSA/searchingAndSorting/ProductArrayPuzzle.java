0341cf2b6621d2c0167fa18e7a02fa35baf5af85
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-04 18:38:44 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/searchingAndSorting/ProductArrayPuzzle.java
+++ src/final450DSA/searchingAndSorting/ProductArrayPuzzle.java
@@ -5,15 +5,38 @@
  *   File: ProductArrayPuzzle
  */
 
+/**
+ * GFG & LEETCODE QUESTION 238 =>
+ * <p>
+ * Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the
+ * elements of nums except nums[i].
+ * <p>
+ * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ * <p>
+ * You must write an algorithm that runs in O(n) time and without using the division operation.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,3,4]
+ * Output: [24,12,8,6]
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [-1,1,0,-3,3]
+ * Output: [0,0,9,0,0]
+ */
+
 package final450DSA.searchingAndSorting;
 
 public class ProductArrayPuzzle {
     static class Solution {
         public int[] productExceptItself(int[] nums) {
             int n = nums.length - 1;
+
             if (n == 1) {
                 return new int[]{1};
             }
+
             int[] left = new int[n];
             int[] right = new int[n];
             int[] output = new int[n];

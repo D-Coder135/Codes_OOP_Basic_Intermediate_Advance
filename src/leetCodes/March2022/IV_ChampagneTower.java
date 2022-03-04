ec22c274243f74c7d7a86d34757d9086a5f16cb2
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-04 19:13:34 +0530

    add a static inner class Solution along with a public method champagneTower() having solution code inside its body

--- src/leetCodes/March2022/IV_ChampagneTower.java
+++ src/leetCodes/March2022/IV_ChampagneTower.java
@@ -7,5 +7,28 @@
 
 package leetCodes.March2022;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IV_ChampagneTower {
+    static class Solution {
+        public double champagneTower(int poured, int query_row, int query_glass) {
+            if (poured == 0)
+                return 0;
+
+            List<Double> list = new ArrayList<>();
+            list.add((double) poured);
+
+            while (query_row-- > 0) {
+                List<Double> temp = new ArrayList<>();
+                temp.add(Math.max(((list.get(0) - 1) / 2), 0));
+                for (int i = 1; i < list.size(); i++) {
+                    temp.add(Math.max(((list.get(i - 1) - 1) / 2), 0) + Math.max((list.get(i) - 1) / 2, 0));
+                }
+                temp.add(Math.max(((list.get(0) - 1) / 2), 0));
+                list = new ArrayList<>(temp);
+            }
+            return Math.min(1, list.get(query_glass));
+        }
+    }
 }

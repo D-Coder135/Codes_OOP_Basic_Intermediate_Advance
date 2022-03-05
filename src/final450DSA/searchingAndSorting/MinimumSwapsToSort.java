2cf817b46ff9ff868558a823605a59a4bcc0f30a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-05 23:34:32 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/searchingAndSorting/MinimumSwapsToSort.java
+++ src/final450DSA/searchingAndSorting/MinimumSwapsToSort.java
@@ -5,6 +5,29 @@
  *   File: MinimumSwapsToSort
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * Given an array of n distinct elements. Find the minimum number of swaps required to sort the array in strictly increasing order.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * nums = {2, 8, 5, 4}
+ * Output:
+ * 1
+ * Explanation:
+ * swap 8 with 4.
+ * Example 2:
+ * <p>
+ * Input:
+ * nums = {10, 19, 6, 3, 5}
+ * Output:
+ * 2
+ * Explanation:
+ * swap 10 with 3 and swap 19 with 5.
+ */
+
 package final450DSA.searchingAndSorting;
 
 import java.util.Arrays;

a7e21b3efd6095f46d2fdc08f87ff70894263343
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-05 19:48:12 +0530

    add a static inner class Pair inside the inner class Solution

--- src/final450DSA/searchingAndSorting/MinimumSwapsToSort.java
+++ src/final450DSA/searchingAndSorting/MinimumSwapsToSort.java
@@ -12,6 +12,11 @@ public class MinimumSwapsToSort {
         //Function to find the minimum number of swaps required to sort the array.
         public int minSwaps(int[] nums) {
             // Code here
+
         }
+
+        static class Pair {
+        }
+
     }
 }

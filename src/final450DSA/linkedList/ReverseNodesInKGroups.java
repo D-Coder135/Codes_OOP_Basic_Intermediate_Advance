f8d2cb1df6a6c8c6ede0bcfeb1b248f8c643628c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-23 23:30:21 +0530

    add a static inner class ListNode

--- src/final450DSA/linkedList/ReverseNodesInKGroups.java
+++ src/final450DSA/linkedList/ReverseNodesInKGroups.java
@@ -8,4 +8,22 @@
 package final450DSA.linkedList;
 
 public class ReverseNodesInKGroups {
+
+    // Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }

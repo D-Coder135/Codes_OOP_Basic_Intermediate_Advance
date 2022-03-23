74ed16d9739fb3988ec381fa337db0e6c2b43c74
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-23 23:54:02 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/ReverseNodesInKGroups.java
+++ src/final450DSA/linkedList/ReverseNodesInKGroups.java
@@ -5,6 +5,28 @@
  *   File: ReverseNodesInKGroups
  */
 
+/**
+ * LEETCODE QUESTION 25 =>
+ * <p>
+ * Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+ * <p>
+ * k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is
+ * not a multiple of k then left-out nodes, in the end, should remain as it is.
+ * <p>
+ * You may not alter the values in the list's nodes, only nodes themselves may be changed.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: head = [1,2,3,4,5], k = 2
+ * Output: [2,1,4,3,5]
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: head = [1,2,3,4,5], k = 3
+ * Output: [3,2,1,4,5]
+ */
+
 package final450DSA.linkedList;
 
 public class ReverseNodesInKGroups {

af999892a17b8bf3957bbc584914fa39533352c2
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-23 18:39:14 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/ReverseLinkedList.java
+++ src/final450DSA/linkedList/ReverseLinkedList.java
@@ -5,6 +5,27 @@
  *   File: ReverseLinkedList
  */
 
+/**
+ * LEETCODE QUESTION 206 =>
+ * <p>
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: head = [1,2]
+ * Output: [2,1]
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: head = []
+ * Output: []
+ */
+
 package final450DSA.linkedList;
 
 public class ReverseLinkedList {

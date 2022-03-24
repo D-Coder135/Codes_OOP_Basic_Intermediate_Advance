406fac363475a79bd24117edc754317f9718f962
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-24 14:56:24 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/CycleInLinkedList.java
+++ src/final450DSA/linkedList/CycleInLinkedList.java
@@ -5,6 +5,31 @@
  *   File: CycleInLinkedList
  */
 
+/**
+ * GFG & LEETCODE QUESTION 141 =>
+ * <p>
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ * <p>
+ * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously
+ * following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer
+ * is connected to. Note that pos is not passed as a parameter.
+ * <p>
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: head = [1,2], pos = 0
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+ */
+
 package final450DSA.linkedList;
 
 public class CycleInLinkedList {

b8399b4c9e26924352765569c4e0e81cc3f10966
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-24 15:28:18 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/NodeWhereCycleBegins.java
+++ src/final450DSA/linkedList/NodeWhereCycleBegins.java
@@ -5,6 +5,31 @@
  *   File: NodeWhereCycleBegins
  */
 
+/**
+ * LEETCODE QUESTION 142 =>
+ * <p>
+ * Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+ * <p>
+ * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously
+ * following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer
+ * is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+ * <p>
+ * Do not modify the linked list.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: tail connects to node index 1
+ * Explanation: There is a cycle in the linked list, where tail connects to the second node.
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: head = [1,2], pos = 0
+ * Output: tail connects to node index 0
+ * Explanation: There is a cycle in the linked list, where tail connects to the first node.
+ */
+
 package final450DSA.linkedList;
 
 public class NodeWhereCycleBegins {

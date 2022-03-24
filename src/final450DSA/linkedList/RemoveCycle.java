b77d07b7e150d9fd0b6be0e12f9fbabf06a1db77
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-24 15:46:36 +0530

    add a static inner class Solution

--- src/final450DSA/linkedList/RemoveCycle.java
+++ src/final450DSA/linkedList/RemoveCycle.java
@@ -14,4 +14,11 @@ public class RemoveCycle {
         Node next;
     }
 
+    static class Solution {
+        //Function to remove a loop in the linked list.
+        public static void removeLoop(Node head) {
+            // code here
+            // remove the loop without losing any nodes
+        }
+    }
 }

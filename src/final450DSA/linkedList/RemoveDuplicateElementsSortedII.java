bab5578364adb350b51d042b99afe475bc7fe968
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-25 15:46:25 +0530

    add a static inner class ListNode

--- src/final450DSA/linkedList/RemoveDuplicateElementsSortedII.java
+++ src/final450DSA/linkedList/RemoveDuplicateElementsSortedII.java
@@ -8,4 +8,21 @@
 package final450DSA.linkedList;
 
 public class RemoveDuplicateElementsSortedII {
+    // Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }

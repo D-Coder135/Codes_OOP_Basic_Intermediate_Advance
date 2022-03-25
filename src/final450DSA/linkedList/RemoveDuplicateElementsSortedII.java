9612edffafd56aee11ca21b6c1cb870aa7293b9f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-25 15:51:17 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/RemoveDuplicateElementsSortedII.java
+++ src/final450DSA/linkedList/RemoveDuplicateElementsSortedII.java
@@ -5,6 +5,24 @@
  *   File: RemoveDuplicateElementsSortedII
  */
 
+/**
+ * LEETCODE QUESTION 82 =>
+ * <p>
+ * Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct
+ * numbers from the original list. Return the linked list sorted as well.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: head = [1,2,3,3,4,4,5]
+ * Output: [1,2,5]
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: head = [1,1,1,2,3]
+ * Output: [2,3]
+ */
+
 package final450DSA.linkedList;
 
 public class RemoveDuplicateElementsSortedII {

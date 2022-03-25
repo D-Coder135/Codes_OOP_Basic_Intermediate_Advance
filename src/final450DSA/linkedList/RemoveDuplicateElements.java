d8da8831be6bfbad92b030173698c7a1a4fb2b1b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-25 15:02:56 +0530

    add a static inner class Solution along with a public method deleteDuplicates() of type ListNode

--- src/final450DSA/linkedList/RemoveDuplicateElements.java
+++ src/final450DSA/linkedList/RemoveDuplicateElements.java
@@ -26,4 +26,10 @@ public class RemoveDuplicateElements {
             this.next = next;
         }
     }
+
+    static class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+
+        }
+    }
 }

c05ba9be450c0dc4384eac42ecab193081cb1d9e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-26 10:31:54 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/MoveLastElementToFront.java
+++ src/final450DSA/linkedList/MoveLastElementToFront.java
@@ -5,6 +5,13 @@
  *   File: MoveLastElementToFront
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * Write a function that moves the last element to the front in a given Singly Linked List. For example,
+ * if the given Linked List is 1->2->3->4->5, then the function should change the list to 5->1->2->3->4.
+ */
+
 package final450DSA.linkedList;
 
 public class MoveLastElementToFront {

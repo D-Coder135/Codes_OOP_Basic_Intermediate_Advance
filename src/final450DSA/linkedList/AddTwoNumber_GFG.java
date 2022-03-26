89e54b8cc50264065f157e526bf9b39b2c5e4081
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-26 11:35:09 +0530

    add a static inner class Solution along with the solution code inside it

--- src/final450DSA/linkedList/AddTwoNumber_GFG.java
+++ src/final450DSA/linkedList/AddTwoNumber_GFG.java
@@ -17,4 +17,55 @@ public class AddTwoNumber_GFG {
             next = null;
         }
     }
+
+    static class Solution {
+
+        static Node reverse(Node node) {
+            Node prev = null;
+            Node current = node;
+            Node next = null;
+            while (current != null) {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+            node = prev;
+            return node;
+        }
+
+        //Function to add two numbers represented by linked list.
+        static Node addTwoLists(Node first, Node second) {
+            // code here
+            // return head of sum list
+            Node temp = new Node(0);
+            Node ans = temp;
+            int carry = 0;
+
+            Node l1 = reverse(first);
+            Node l2 = reverse(second);
+
+            while (l1 != null || l2 != null) {
+                int sum = 0;
+                if (l1 != null) {
+                    sum += l1.data;
+                    l1 = l1.next;
+                }
+                if (l2 != null) {
+                    sum += l2.data;
+                    l2 = l2.next;
+                }
+
+                sum += carry;
+
+                temp.next = new Node(sum % 10);
+                carry = sum / 10;
+                temp = temp.next;
+            }
+            if (carry != 0) {
+                temp.next = new Node(carry);
+            }
+            return reverse(ans.next);
+        }
+    }
 }

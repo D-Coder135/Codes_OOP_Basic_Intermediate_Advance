53eb1a6e724897ec9a3fe20b6e922da4a8aa7456
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-26 10:23:28 +0530

    add a new class MoveLastElementToFront.java inside the package final450DSA.linkedList

---
+++ src/final450DSA/linkedList/MoveLastElementToFront.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 26-03-2022/03/2022
+ *   Time: 10:22 AM
+ *   File: MoveLastElementToFront
+ */
+
+package final450DSA.linkedList;
+
+public class MoveLastElementToFront {
+}

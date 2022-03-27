73230438f357d701394a4a7e2907151b2b54a069
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-27 13:41:33 +0530

    add a public static inner class ListNode

--- src/final450DSA/linkedList/IntersectionOfTwoLinkedList__LEETCODE.java
+++ src/final450DSA/linkedList/IntersectionOfTwoLinkedList__LEETCODE.java
@@ -8,4 +8,13 @@
 package final450DSA.linkedList;
 
 public class IntersectionOfTwoLinkedList__LEETCODE {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
 }

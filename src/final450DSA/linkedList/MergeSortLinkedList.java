99804cd59b54fe0e4d84c1ae4b722df327b0ef0e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-28 19:55:10 +0530

    update the fields slow and fast

--- src/final450DSA/linkedList/MergeSortLinkedList.java
+++ src/final450DSA/linkedList/MergeSortLinkedList.java
@@ -29,7 +29,8 @@ public class MergeSortLinkedList {
             Node slow = head, fast = head;
 
             while (fast.next != null && fast.next.next != null) {
-
+                slow = slow.next;
+                fast = fast.next.next;
             }
         }
     }

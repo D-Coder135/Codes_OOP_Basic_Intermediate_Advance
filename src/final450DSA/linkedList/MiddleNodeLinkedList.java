8c64067ac063d32efb24b9497c99ee55f6fc58ab
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-29 13:48:03 +0530

    add a new class MiddleNodeLinkedList.java inside the package final450DSA.linkedList

---
+++ src/final450DSA/linkedList/MiddleNodeLinkedList.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 29-03-2022/03/2022
+ *   Time: 01:47 PM
+ *   File: MiddleOfLinkedList
+ */
+
+package final450DSA.linkedList;
+
+public class MiddleNodeLinkedList {
+}

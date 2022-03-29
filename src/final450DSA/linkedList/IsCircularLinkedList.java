68df4abbaa88d4bb03ee6465f5aecb935adbd979
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-29 14:12:46 +0530

    add a new class IsCircularLinkedList.java inside the package final450DSA.linkedList

---
+++ src/final450DSA/linkedList/IsCircularLinkedList.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 29-03-2022/03/2022
+ *   Time: 02:12 PM
+ *   File: IsCircularLinkedList
+ */
+
+package final450DSA.linkedList;
+
+public class IsCircularLinkedList {
+}

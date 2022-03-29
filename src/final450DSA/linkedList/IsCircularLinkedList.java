950bfbd2a39384d7408b1720e7700acbb342379c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-29 14:17:44 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/IsCircularLinkedList.java
+++ src/final450DSA/linkedList/IsCircularLinkedList.java
@@ -5,6 +5,31 @@
  *   File: IsCircularLinkedList
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * Given head, the head of a singly linked list, find if the linked list is circular or not.
+ * A linked list is called circular if it is not NULL terminated and all nodes are connected in the form of a cycle.
+ * An empty linked list is considered as circular.
+ * <p>
+ * Note: The linked list does not contain any inner loop.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * LinkedList: 1->2->3->4->5
+ * (the first and last node is connected,
+ * i.e. 5 --> 1)
+ * Output: 1
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input:
+ * LinkedList: 2->4->6->7->5->1
+ * Output: 0
+ */
+
 package final450DSA.linkedList;
 
 public class IsCircularLinkedList {

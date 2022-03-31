77b668b7607df934da3fd65510f2929dbdc81bf0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-03-31 10:10:23 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/CountTripletsEqualToGivenSumDLL.java
+++ src/final450DSA/linkedList/CountTripletsEqualToGivenSumDLL.java
@@ -5,6 +5,13 @@
  *   File: CountTripletsEqualToGivenSumDLL
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * Given a sorted doubly linked list of distinct nodes(no two nodes have the same data) and a value x.
+ * Count triplets in the list that sum up to a given value x.
+ */
+
 package final450DSA.linkedList;
 
 public class CountTripletsEqualToGivenSumDLL {

00dce19372f0480bcdae4175ce29a6d211109927
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-01 22:11:18 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/SortKSortedDLL.java
+++ src/final450DSA/linkedList/SortKSortedDLL.java
@@ -5,6 +5,14 @@
  *   File: SortKSortedDLL
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * Given a doubly linked list containing n nodes, where each node is at most k away from its target position in the
+ * list. The problem is to sort the given doubly linked list. For example, let us consider k is 2, a node at
+ * position 7 in the sorted doubly linked list, can be at positions 5, 6, 7, 8, 9 in the given doubly linked list.
+ */
+
 package final450DSA.linkedList;
 
 import java.util.Comparator;

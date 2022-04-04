d1de39269ea687946983ce3c8ebe7fae8753aa97
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-04 22:05:30 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/RotateListDLLByPNodes.java
+++ src/final450DSA/linkedList/RotateListDLLByPNodes.java
@@ -5,6 +5,25 @@
  *   File: RotateListDLLByPNodes
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * Given a doubly linked list, rotate the linked list counter-clockwise by P nodes. Here P is a given positive integer
+ * and is smaller than the count of nodes(N) in a linked list.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 1 <-> 2 <-> 3 <-> 4 <-> 5 <-> 6 , P = 2
+ * Output: 3 <-> 4 <-> 5 <-> 6 <-> 1 <-> 2
+ * Explanation: Doubly linked list after rotating
+ * 2 nodes is: 3 4 5 6 1 2.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: 3 <-> 4 <-> 5 <-> 1 ,P = 3
+ * Output: 1<-> 3 <-> 4 <-> 5
+ */
+
 package final450DSA.linkedList;
 
 public class RotateListDLLByPNodes {

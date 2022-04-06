3c4abd2ce7a620f7293c924b6802a6228186a342
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-06 11:26:21 +0530

    add a static inner class Node

---
+++ src/final450DSA/linkedList/FlatternALinkedList.java
@@ -0,0 +1,22 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 06-04-2022/04/2022
+ *   Time: 11:24 AM
+ *   File: FlatternALinkedList
+ */
+
+package final450DSA.linkedList;
+
+public class FlatternALinkedList {
+    static class Node {
+        int data;
+        Node next;
+        Node bottom;
+
+        Node(int d) {
+            data = d;
+            next = null;
+            bottom = null;
+        }
+    }
+}

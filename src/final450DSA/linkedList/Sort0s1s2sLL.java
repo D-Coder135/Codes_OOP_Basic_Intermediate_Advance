d6bb661cbbff032efdf2a162a484bdfb8639ca6e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-07 23:59:53 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/Sort0s1s2sLL.java
+++ src/final450DSA/linkedList/Sort0s1s2sLL.java
@@ -5,6 +5,36 @@
  *   File: Sort0s1s2sLL
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * Given a linked list of N nodes where nodes can contain values 0s, 1s, and 2s only. The task is to segregate 0s,
+ * 1s, and 2s linked list such that all zeros segregate to head side, 2s at the end of the linked list,
+ * and 1s in the mid of 0s and 2s.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * N = 8
+ * value[] = {1,2,2,1,2,0,2,2}
+ * Output: 0 1 1 2 2 2 2 2
+ * Explanation: All the 0s are segregated
+ * to the left end of the linked list,
+ * 2s to the right end of the list, and
+ * 1s in between.
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input:
+ * N = 4
+ * value[] = {2,2,0,1}
+ * Output: 0 1 2 2
+ * Explanation: After arranging all the
+ * 0s,1s and 2s in the given format,
+ * the output will be 0 1 2 2.
+ */
+
 package final450DSA.linkedList;
 
 public class Sort0s1s2sLL {

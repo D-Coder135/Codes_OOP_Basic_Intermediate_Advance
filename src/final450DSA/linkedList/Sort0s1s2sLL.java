bf1d8820146a72bcf8348a71b142ddf55095069b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-07 16:40:39 +0530

    add a static inner class Solution along with a static method segregate() of type Node

--- src/final450DSA/linkedList/Sort0s1s2sLL.java
+++ src/final450DSA/linkedList/Sort0s1s2sLL.java
@@ -17,4 +17,12 @@ public class Sort0s1s2sLL {
             next = null;
         }
     }
+
+    static class Solution {
+        //Function to sort a linked list of 0s, 1s and 2s.
+        static Node segregate(Node head) {
+            // add your code here
+        }
+    }
+
 }

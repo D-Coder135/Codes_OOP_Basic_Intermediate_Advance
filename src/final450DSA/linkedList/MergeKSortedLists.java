c340887b1e473320fdf34243237e4a1322b7ae0c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-09 13:05:44 +0530

    add while loop inside the for loop

--- src/final450DSA/linkedList/MergeKSortedLists.java
+++ src/final450DSA/linkedList/MergeKSortedLists.java
@@ -37,6 +37,9 @@ public class MergeKSortedLists {
 
             for (int i = 0; i < lists.length; i++) {
                 ListNode curr = lists[i];
+                while (curr != null) {
+
+                }
             }
         }
     }

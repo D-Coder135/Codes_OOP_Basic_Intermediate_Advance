2c599ff8c7be053e5edaa2563d9294dfb0953052
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-10 23:34:48 +0530

    add while loop

--- src/final450DSA/linkedList/DeleteNodesHavingGreaterValueOnRight.java
+++ src/final450DSA/linkedList/DeleteNodesHavingGreaterValueOnRight.java
@@ -22,6 +22,13 @@ public class DeleteNodesHavingGreaterValueOnRight {
             // your code here
             Node curr = head;
             Node prev = null;
+
+            while (curr != null) {
+                Node temp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = temp;
+            }
         }
     }
 }

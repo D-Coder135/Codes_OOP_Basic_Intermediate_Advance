a86f692aa7c90e497731d063054a2599dd0a9df9
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-10 23:45:28 +0530

    add a new class RemoveLinkedListElement.java inside the package final450DSA.linkedList

---
+++ src/final450DSA/linkedList/RemoveLinkedListElement.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 10-04-2022/04/2022
+ *   Time: 11:45 PM
+ *   File: RemoveLinkedListElement
+ */
+
+package final450DSA.linkedList;
+
+public class RemoveLinkedListElement {
+}

7cbde3861521095a78706425cc0eec164d25b184
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-10 23:44:33 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/DeleteNodesHavingGreaterValueOnRight.java
+++ src/final450DSA/linkedList/DeleteNodesHavingGreaterValueOnRight.java
@@ -5,6 +5,30 @@
  *   File: DeleteNodesHavingGreaterValueOnRight
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * Given a singly linked list, remove all the nodes which have a greater value on their right side.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * LinkedList = 12->15->10->11->5->6->2->3
+ * Output: 15 11 6 3
+ * Explanation: Since, 12, 10, 5 and 2 are
+ * the elements which have greater elements
+ * on the following nodes. So, after deleting
+ * them, the linked list would like be 15,
+ * 11, 6, 3.
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * Input:
+ * LinkedList = 10->20->30->40->50->60
+ * Output: 60
+ */
+
 package final450DSA.linkedList;
 
 public class DeleteNodesHavingGreaterValueOnRight {

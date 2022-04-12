90e877e910e91c59863feb2169a216a046150ed8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-12 22:44:38 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/RemoveNthNodeFromEnd.java
+++ src/final450DSA/linkedList/RemoveNthNodeFromEnd.java
@@ -5,6 +5,27 @@
  *   File: RemoveNthNode
  */
 
+/**
+ * LEETCODE QUESTION 19 =>
+ * <p>
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: head = [1,2,3,4,5], n = 2
+ * Output: [1,2,3,5]
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: head = [1], n = 1
+ * Output: []
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: head = [1,2], n = 1
+ * Output: [1]
+ */
+
 package final450DSA.linkedList;
 
 public class RemoveNthNodeFromEnd {

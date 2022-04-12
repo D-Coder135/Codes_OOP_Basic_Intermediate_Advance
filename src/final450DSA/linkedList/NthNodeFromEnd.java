7600d667780af747bf0814691636d58a1486c728
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-12 22:41:45 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/NthNodeFromEnd.java
+++ src/final450DSA/linkedList/NthNodeFromEnd.java
@@ -5,6 +5,36 @@
  *   File: NthNodeFromEnd
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * Given a linked list consisting of L nodes and given a number N. The task is to find the Nth node from
+ * the end of the linked list.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * N = 2
+ * LinkedList: 1->2->3->4->5->6->7->8->9
+ * Output: 8
+ * Explanation: In the first example, there
+ * are 9 nodes in linked list and we need
+ * to find 2nd node from end. 2nd node
+ * from end os 8.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input:
+ * N = 5
+ * LinkedList: 10->5->100->5
+ * Output: -1
+ * Explanation: In the second example, there
+ * are 4 nodes in the linked list and we
+ * need to find 5th from the end. Since 'n'
+ * is more than the number of nodes in the
+ * linked list, the output is -1.
+ */
+
 package final450DSA.linkedList;
 
 public class NthNodeFromEnd {

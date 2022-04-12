f836d642f58fe443f078315c4aea80a4952e3590
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-12 21:47:57 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/SegregateEvenOddNodes.java
+++ src/final450DSA/linkedList/SegregateEvenOddNodes.java
@@ -5,6 +5,42 @@
  *   File: SegregateEvenOddNodes
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * Given a link list of size N, modify the list such that all the even numbers appear before all the odd numbers
+ * in the modified list. The order of appearance of numbers within each segregation should be same as
+ * that in the original list.
+ * <p>
+ * NOTE: Don't create a new linked list, instead rearrange the provided one.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * N = 7
+ * Link List:
+ * 17 -> 15 -> 8 -> 9 -> 2 -> 4 -> 6 -> NULL
+ * <p>
+ * Output: 8 2 4 6 17 15 9
+ * <p>
+ * Explanation: 8,2,4,6 are the even numbers,
+ * so they appear first and 17,15,9 are odd
+ * numbers that appear later.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input:
+ * N = 4
+ * Link List:
+ * 1 -> 3 -> 5 -> 7
+ * <p>
+ * Output: 1 3 5 7
+ * <p>
+ * Explanation: There is no even number.
+ * So né need for modification.
+ */
+
 package final450DSA.linkedList;
 
 public class SegregateEvenOddNodes {

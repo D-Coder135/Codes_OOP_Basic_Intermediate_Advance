e6d12b7370278c73619565322cc42e1491ed492f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-12 22:19:01 +0530

    add a static inner class Node

--- src/final450DSA/linkedList/NthNodeFromEnd.java
+++ src/final450DSA/linkedList/NthNodeFromEnd.java
@@ -8,4 +8,13 @@
 package final450DSA.linkedList;
 
 public class NthNodeFromEnd {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
 }

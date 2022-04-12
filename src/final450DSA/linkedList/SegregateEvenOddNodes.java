a70872099e354395b0726e4b72a7820e84f38d1c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-12 21:38:23 +0530

    add a new class SegregateEvenOddNodes.java inside the package final450DSA.linkedList

---
+++ src/final450DSA/linkedList/SegregateEvenOddNodes.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 12-04-2022/04/2022
+ *   Time: 09:37 PM
+ *   File: SegregateEvenOddNodes
+ */
+
+package final450DSA.linkedList;
+
+public class SegregateEvenOddNodes {
+}

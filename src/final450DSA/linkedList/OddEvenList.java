0b153fafc90c50d6981916d4f2731fc941f7c355
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-12 22:01:17 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/linkedList/OddEvenList.java
+++ src/final450DSA/linkedList/OddEvenList.java
@@ -5,6 +5,29 @@
  *   File: OddEvenList
  */
 
+/**
+ * LEETCODE QUESTION 328 =>
+ * <p>
+ * Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes
+ * with even indices, and return the reordered list.
+ * <p>
+ * The first node is considered odd, and the second node is even, and so on.
+ * <p>
+ * Note that the relative order inside both the even and odd groups should remain as it was in the input.
+ * <p>
+ * You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: head = [1,2,3,4,5]
+ * Output: [1,3,5,2,4]
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: head = [2,1,3,5,6,4,7]
+ * Output: [2,3,6,7,1,5,4]
+ */
+
 package final450DSA.linkedList;
 
 public class OddEvenList {

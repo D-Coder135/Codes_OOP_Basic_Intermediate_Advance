7a26bceba8c7ba585f020b17812bce83db9ed68f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-13 22:40:46 +0530

    add the solution code inside the method firstUniqChar() body

--- src/final450DSA/linkedList/FirstUniqueCharacter.java
+++ src/final450DSA/linkedList/FirstUniqueCharacter.java
@@ -7,12 +7,30 @@
 
 package final450DSA.linkedList;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 public class FirstUniqueCharacter {
     static class Solution {
         public int firstUniqChar(String s) {
             if (s.isEmpty() || s.isBlank()) {
                 return -1;
             }
+            HashMap<Character, Integer> map = new HashMap<>();
+            int indexAns = -1;
+
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+            }
+            for (int i = 0; i < s.length(); i++) {
+                if (map.get(s.charAt(i)) == 1) {
+                    return i;
+                }
+            }
+            return -1;
         }
     }
 }

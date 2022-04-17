a268643b1fbec382850c23675e240b2832700287
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-17 11:50:56 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/stackAndQueues/ImplementTwoStacksInAnArray.java
+++ src/final450DSA/stackAndQueues/ImplementTwoStacksInAnArray.java
@@ -5,6 +5,35 @@
  *   File: ImplementTwoStacksInAnArray
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * Your task is to implement  2 stacks in one array efficiently.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * push1(2)
+ * push1(3)
+ * push2(4)
+ * pop1()
+ * pop2()
+ * pop2()
+ * <p>
+ * Output:
+ * 3 4 -1
+ * <p>
+ * Explanation:
+ * push1(2) the stack1 will be {2}
+ * push1(3) the stack1 will be {2,3}
+ * push2(4) the stack2 will be {4}
+ * pop1()   the poped element will be 3
+ * from stack1 and stack1 will be {2}
+ * pop2()   the poped element will be 4
+ * from stack2 and now stack2 is empty
+ * pop2()   the stack2 is now empty hence -1 .
+ */
+
 package final450DSA.stackAndQueues;
 
 public class ImplementTwoStacksInAnArray {

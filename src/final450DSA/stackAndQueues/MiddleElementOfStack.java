9d2964325b5b1a6569c49748743d524b35e58b3e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-17 12:18:39 +0530

    add a static inner class DLLNode along with some private fields

---
+++ src/final450DSA/stackAndQueues/MiddleElementOfStack.java
@@ -0,0 +1,18 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 17-04-2022/04/2022
+ *   Time: 12:16 PM
+ *   File: MiddleElementOfStack
+ */
+
+package final450DSA.stackAndQueues;
+
+public class MiddleElementOfStack {
+    static class DLLNode {
+        DLLNode prev;
+        DLLNode next;
+        int data;
+
+
+    }
+}

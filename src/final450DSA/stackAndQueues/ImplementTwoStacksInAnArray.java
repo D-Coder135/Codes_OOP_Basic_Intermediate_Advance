0ef9c39b2a8049f3ed92f29e9d0db3059cd3a737
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-17 11:45:14 +0530

    add a static inner class TwoStack

--- src/final450DSA/stackAndQueues/ImplementTwoStacksInAnArray.java
+++ src/final450DSA/stackAndQueues/ImplementTwoStacksInAnArray.java
@@ -8,4 +8,16 @@
 package final450DSA.stackAndQueues;
 
 public class ImplementTwoStacksInAnArray {
+    static class TwoStack {
+
+        int size;
+        int top1, top2;
+        int[] arr = new int[100];
+
+        TwoStack() {
+            size = 100;
+            top1 = -1;
+            top2 = size;
+        }
+    }
 }

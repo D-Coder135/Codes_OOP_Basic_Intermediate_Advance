ae28935f82a2c2ede54a663a1861c6d11ccce338
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-17 13:02:56 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/stackAndQueues/MiddleElementOfStack.java
+++ src/final450DSA/stackAndQueues/MiddleElementOfStack.java
@@ -5,6 +5,23 @@
  *   File: MiddleElementOfStack
  */
 
+/**
+ * GFG QUESTION =>
+ * <p>
+ * How to implement a stack which will support following operations in O(1) time complexity?
+ * 1) push() which adds an element to the top of stack.
+ * 2) pop() which removes an element from top of stack.
+ * 3) findMiddle() which will return middle element of the stack.
+ * 4) deleteMiddle() which will delete the middle element.
+ * Push and pop are standard stack operations.
+ * The important question is, whether to use a linked list or array for the implementation of the stack?
+ * Please note that we need to find and delete the middle element. Deleting an element from the middle is not O(1)
+ * for array. Also, we may need to move the middle pointer up when we push an element and move down when we pop().
+ * In a singly linked list, moving the middle pointer in both directions is not possible.
+ * The idea is to use Doubly Linked List (DLL). We can delete the middle element in O(1) time by
+ * maintaining mid-pointer. We can move mid-pointer in both directions using previous and next pointers.
+ */
+
 package final450DSA.stackAndQueues;
 
 public class MiddleElementOfStack {

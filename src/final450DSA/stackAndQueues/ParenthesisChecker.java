0c1aef2242f753858d22b8bd39dc8f141d4922a4
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-19 00:07:18 +0530

    add an object of type Stack<Character>

--- src/final450DSA/stackAndQueues/ParenthesisChecker.java
+++ src/final450DSA/stackAndQueues/ParenthesisChecker.java
@@ -7,11 +7,14 @@
 
 package final450DSA.stackAndQueues;
 
+import java.util.Stack;
+
 public class ParenthesisChecker {
     static class Solution {
         //Function to check if brackets are balanced or not.
         static boolean ispar(String x) {
             // add your code here
+            Stack<Character> stack = new Stack<>();
         }
     }
 }

5a2d5ff41516bdc980092272398c649696ab6e66
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-19 00:01:51 +0530

    add a static inner class Solution along with a method ispar() of type boolean

--- src/final450DSA/stackAndQueues/ParenthesisChecker.java
+++ src/final450DSA/stackAndQueues/ParenthesisChecker.java
@@ -8,4 +8,10 @@
 package final450DSA.stackAndQueues;
 
 public class ParenthesisChecker {
+    static class Solution {
+        //Function to check if brackets are balanced or not.
+        static boolean ispar(String x) {
+            // add your code here
+        }
+    }
 }

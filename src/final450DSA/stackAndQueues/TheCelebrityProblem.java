ec275e1fa34ef7bdddebb2d0e31069bab2b5fd92
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-20 20:14:21 +0530

    add a for loop

--- src/final450DSA/stackAndQueues/TheCelebrityProblem.java
+++ src/final450DSA/stackAndQueues/TheCelebrityProblem.java
@@ -32,6 +32,10 @@ public class TheCelebrityProblem {
             }
 
             int potentialAns = stack.pop();
+
+            for (int i = 0; i < arr.length; i++) {
+                
+            }
         }
     }
 }

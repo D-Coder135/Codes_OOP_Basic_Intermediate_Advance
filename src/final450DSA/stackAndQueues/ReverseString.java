dc131f54aca85b6d55f691e628dd96f9a0ae08e8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-20 18:32:12 +0530

    call the push() method

--- src/final450DSA/stackAndQueues/ReverseString.java
+++ src/final450DSA/stackAndQueues/ReverseString.java
@@ -14,7 +14,7 @@ public class ReverseString {
         public void reverseString(char[] s) {
             Stack<Character> stack = new Stack<>();
             for (int i = 0; i < s.length; i++) {
-
+                stack.push(s[i]);
             }
         }
     }

b10bedba071629a842504bf30090778e23a96b08
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-20 19:13:40 +0530

    add a new class NextGreaterElement1.java inside the package final450DSA.stackAndQueues

---
+++ src/final450DSA/stackAndQueues/NextGreaterElement1.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 20-04-2022/04/2022
+ *   Time: 07:13 PM
+ *   File: NextGreaterElement1
+ */
+
+package final450DSA.stackAndQueues;
+
+public class NextGreaterElement1 {
+}

fc8b18924a3ba39b55d3237211e44c76007b0e0e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-21 19:11:44 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/stackAndQueues/ReversePolishNotation.java
+++ src/final450DSA/stackAndQueues/ReversePolishNotation.java
@@ -5,6 +5,42 @@
  *   File: ReversePolishNotation
  */
 
+/**
+ * LEETCODE QUESTION 150 =>
+ * <p>
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ * Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+ * <p>
+ * Note that division between two integers should truncate toward zero.
+ * It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate
+ * to a result, and there will not be any division by zero operation.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: tokens = ["2","1","+","3","*"]
+ * Output: 9
+ * Explanation: ((2 + 1) * 3) = 9
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: tokens = ["4","13","5","/","+"]
+ * Output: 6
+ * Explanation: (4 + (13 / 5)) = 6
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+ * Output: 22
+ * Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+ * = ((10 * (6 / (12 * -11))) + 17) + 5
+ * = ((10 * (6 / -132)) + 17) + 5
+ * = ((10 * 0) + 17) + 5
+ * = (0 + 17) + 5
+ * = 17 + 5
+ * = 22
+ */
+
 package final450DSA.stackAndQueues;
 
 import java.util.Stack;

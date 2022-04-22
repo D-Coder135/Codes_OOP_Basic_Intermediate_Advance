d90129de1755818b27ccabe42c43e8c62662bdf0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-22 16:29:21 +0530

    add a private static method insertBottom() of type void

--- src/final450DSA/stackAndQueues/ReverseAStack.java
+++ src/final450DSA/stackAndQueues/ReverseAStack.java
@@ -18,4 +18,8 @@ public class ReverseAStack {
         reverseStack(stack);
         insertBottom(stack, item);
     }
+
+    private static void insertBottom(Stack<Integer> stack, int item) {
+
+    }
 }

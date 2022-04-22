3774580ea39f8c91f9254c32d42cf70f2e3065d6
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-22 16:53:13 +0530

    add a static inner class GFG along with a public method sort() of type Stack<Integer>

---
+++ src/final450DSA/stackAndQueues/SortAStack.java
@@ -0,0 +1,18 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 22-04-2022/04/2022
+ *   Time: 04:41 PM
+ *   File: SortAStack
+ */
+
+package final450DSA.stackAndQueues;
+
+import java.util.Stack;
+
+public class SortAStack {
+    static class GfG {
+        public Stack<Integer> sort(Stack<Integer> s) {
+            //add code here.
+        }
+    }
+}

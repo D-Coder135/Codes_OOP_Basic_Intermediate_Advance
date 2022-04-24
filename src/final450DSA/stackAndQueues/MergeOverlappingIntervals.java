aade0e8a4e727fe5204bbefd05faac9885deaad1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-24 22:15:05 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/stackAndQueues/MergeOverlappingIntervals.java
+++ src/final450DSA/stackAndQueues/MergeOverlappingIntervals.java
@@ -5,6 +5,25 @@
  *   File: MergeOverlappingIntervals
  */
 
+/**
+ * LEETCODE QUESTION 56 =>
+ * <p>
+ * Given an array of intervals where intervals[i] = [start i, end i], merge all overlapping intervals,
+ * and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * Output: [[1,6],[8,10],[15,18]]
+ * Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: intervals = [[1,4],[4,5]]
+ * Output: [[1,5]]
+ * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ */
+
 package final450DSA.stackAndQueues;
 
 import java.util.Arrays;

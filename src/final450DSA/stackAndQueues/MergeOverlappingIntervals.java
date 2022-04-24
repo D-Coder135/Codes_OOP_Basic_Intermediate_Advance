92005b82fd69d4f976df5b812c6a825175a7b8f3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-24 22:10:02 +0530

    call the Arrays.sort() method

--- src/final450DSA/stackAndQueues/MergeOverlappingIntervals.java
+++ src/final450DSA/stackAndQueues/MergeOverlappingIntervals.java
@@ -15,7 +15,7 @@ import java.util.Stack;
 public class MergeOverlappingIntervals {
     static class Solution {
         public int[][] merge(int[][] intervals) {
-
+            Arrays.sort(intervals, (int[] a, int[] b) -> Integer.compare(a[0], b[0]));
         }
 
     }

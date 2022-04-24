ec0b6316332360247010a7553a1809ec460c0f6c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-24 22:09:53 +0530

    remove some code

--- src/final450DSA/stackAndQueues/MergeOverlappingIntervals.java
+++ src/final450DSA/stackAndQueues/MergeOverlappingIntervals.java
@@ -9,51 +9,13 @@ package final450DSA.stackAndQueues;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Stack;
 
 public class MergeOverlappingIntervals {
     static class Solution {
         public int[][] merge(int[][] intervals) {
-            Pair[] pairs = new Pair[intervals.length];
-            ArrayList<int[]> ans = new ArrayList<>();
-            for (int i = 0; i < intervals.length; i++) {
-                pairs[i] = new Pair(intervals[i][0], intervals[i][1]);
-            }
-            Stack<Pair> st = new Stack<>();
-            for (int i = 0; i < pairs.length; i++) {
-                if (i == 0) {
-                    st.push(pairs[i]);
-                } else {
-                    Pair top = st.peek();
 
-                    if (pairs[i].start > top.end) {
-                        st.push(pairs[i]);
-                    } else {
-                        top.end = Math.max(top.end, pairs[i].end);
-                    }
-                }
-            }
-
-            Stack<Pair> rs = new Stack<>();
-            while (st.size() > 0) {
-                rs.push(st.pop());
-            }
-
-            while (rs.size() > 0) {
-                Pair p = rs.pop();
-                ans.add(new int[]{p.start, p.end});
-            }
-            return ans.toArray(new int[0][]);
-        }
-
-        public static class Pair {
-            int start;
-            int end;
-
-            public Pair(int start, int end) {
-                this.start = start;
-                this.end = end;
-            }
         }
 
     }

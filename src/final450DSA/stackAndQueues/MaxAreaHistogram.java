61edd87bbf7bd11e9adeed804072c9e4352ecb71
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-24 22:51:00 +0530

    add a static inner class Solution along with the solution code inside it

--- src/final450DSA/stackAndQueues/MaxAreaHistogram.java
+++ src/final450DSA/stackAndQueues/MaxAreaHistogram.java
@@ -7,5 +7,48 @@
 
 package final450DSA.stackAndQueues;
 
+import java.util.Stack;
+
 public class MaxAreaHistogram {
+    static class Solution {
+        //Function to find largest rectangular area possible in a given histogram.
+        public static long getMaxArea(long arr[], long n) {
+            // your code here
+            Stack<Integer> stack = new Stack<>();
+            long maxArea = 0;
+
+            for (int i = 0; i < arr.length; ) {
+                if (stack.isEmpty() || arr[i] > arr[stack.peek()]) {
+                    stack.push(i);
+                    i++;
+                } else {
+                    int right = i;
+                    long height = arr[stack.pop()];
+                    if (stack.isEmpty()) {
+                        long area = height * right;
+                        maxArea = Math.max(maxArea, area);
+                    } else {
+                        int left = stack.peek();
+                        long area = height * (right - left - 1);
+                        maxArea = Math.max(maxArea, area);
+                    }
+                }
+            }
+
+            int right = arr.length;
+            while (!stack.isEmpty()) {
+                long height = arr[stack.pop()];
+                if (stack.isEmpty()) {
+                    long area = height * right;
+                    maxArea = Math.max(maxArea, area);
+                } else {
+                    int left = stack.peek();
+                    long area = height * (right - left - 1);
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+            return maxArea;
+        }
+
+    }
 }

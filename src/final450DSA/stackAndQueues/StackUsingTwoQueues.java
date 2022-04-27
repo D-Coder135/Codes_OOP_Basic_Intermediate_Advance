f6d5a4a336c57015f05197567a281dcfb05db16b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-27 23:43:25 +0530

    add a static inner class MyStack along with some public methods

--- src/final450DSA/stackAndQueues/StackUsingTwoQueues.java
+++ src/final450DSA/stackAndQueues/StackUsingTwoQueues.java
@@ -8,4 +8,26 @@
 package final450DSA.stackAndQueues;
 
 public class StackUsingTwoQueues {
+    static class MyStack {
+
+        public MyStack() {
+
+        }
+
+        public void push(int x) {
+
+        }
+
+        public int pop() {
+
+        }
+
+        public int top() {
+
+        }
+
+        public boolean empty() {
+
+        }
+    }
 }

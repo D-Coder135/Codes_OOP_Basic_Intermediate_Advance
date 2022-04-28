fbfd7b4faf5b890cf83496b97df60c1fa0033f27
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-28 10:58:24 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/stackAndQueues/ValidateStackSequence.java
+++ src/final450DSA/stackAndQueues/ValidateStackSequence.java
@@ -5,6 +5,29 @@
  *   File: ValidateStackSequence
  */
 
+/**
+ * GFG & LEETCODE QUESTION 946 =>
+ * <p>
+ * Given two integer arrays pushed and popped each with distinct values, return true if this could have been
+ * the result of a sequence of push and pop operations on an initially empty stack, or false otherwise.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+ * Output: true
+ * Explanation: We might do the following sequence:
+ * push(1), push(2), push(3), push(4),
+ * pop() -> 4,
+ * push(5),
+ * pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+ * Output: false
+ * Explanation: 1 cannot be popped before 2.
+ */
+
 package final450DSA.stackAndQueues;
 
 import java.util.Stack;

e475cc9e06963594a4694c808f6ab5ee5ecbec93
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-05-14 21:51:35 +0530

    add the problem statement as comment inside the code

--- src/final450DSA/stackAndQueues/QueueImplementationUsingStack.java
+++ src/final450DSA/stackAndQueues/QueueImplementationUsingStack.java
@@ -5,6 +5,42 @@
  *   File: QueueImplementationUsingStack
  */
 
+/**
+ * GFG & LEETCODE QUESTION 232 =>
+ * <p>
+ * Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the
+ * functions of a normal queue (push, peek, pop, and empty).
+ * <p>
+ * Implement the MyQueue class:
+ * <p>
+ * void push(int x) Pushes element x to the back of the queue.
+ * int pop() Removes the element from the front of the queue and returns it.
+ * int peek() Returns the element at the front of the queue.
+ * boolean empty() Returns true if the queue is empty, false otherwise.
+ * Notes:
+ * <p>
+ * You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is
+ * empty operations are valid.
+ * Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or
+ * deque (double-ended queue) as long as you use only a stack's standard operations.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input
+ * ["MyQueue", "push", "push", "peek", "pop", "empty"]
+ * [[], [1], [2], [], [], []]
+ * Output
+ * [null, null, null, 1, 1, false]
+ * <p>
+ * Explanation
+ * MyQueue myQueue = new MyQueue();
+ * myQueue.push(1); // queue is: [1]
+ * myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+ * myQueue.peek(); // return 1
+ * myQueue.pop(); // return 1, queue is [2]
+ * myQueue.empty(); // return false
+ */
+
 package final450DSA.stackAndQueues;
 
 import java.util.Stack;

ce57cc8edbc4be526817fcf7399efba381c18cc5
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-05-16 14:33:57 +0530

    add the solution code inside the static inner class MyCircularQueue

--- src/final450DSA/stackAndQueues/CircularQueue.java
+++ src/final450DSA/stackAndQueues/CircularQueue.java
@@ -8,4 +8,49 @@
 package final450DSA.stackAndQueues;
 
 public class CircularQueue {
+    static class MyCircularQueue {
+        final int[] arr;
+        int front = 0, rear = -1, size = 0;
+
+        public MyCircularQueue(int k) {
+            arr = new int[k];
+        }
+
+        public boolean enQueue(int value) {
+            if (!isFull()) {
+                rear = (rear + 1) % arr.length;
+                arr[rear] = value;
+                size++;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public boolean deQueue() {
+            if (!isEmpty()) {
+                front = (front + 1) % arr.length;
+                size--;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public int Front() {
+            return isEmpty() ? -1 : arr[front];
+        }
+
+        public int Rear() {
+            return isEmpty() ? -1 : arr[rear];
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public boolean isFull() {
+            return size == arr.length;
+        }
+    }
 }
